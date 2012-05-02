e56d61ada718026870b997c93710be2633eced81
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2012-05-02 15:27:15 +0200

    [MRG] hr_timesheet_task redisign (lp:c2c-addons/6.1  rev 40.1.3)

--- timesheet_task/__openerp__.py
+++ timesheet_task/__openerp__.py
@@ -18,18 +18,18 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-{'name' : 'Task in time sheet',
+{'name' : 'Analytic Task',
  'version' : '0.1',
  'author' : 'Camptocamp',
  'maintainer': 'Camptocamp',
  'category': 'Human Resources',
  'complexity': "normal", #easy, normal, expert
- 'depends' : ['timesheet_task', 'hr_timesheet_sheet'],
+ 'depends' : ['hr_timesheet', 'project', 'hr_timesheet_invoice'],
  'description': """Replace project.task.work items linked to task
                    with hr.analytic.timesheet""",
  'website': 'http://www.camptocamp.com',
  'init_xml': [],
- 'update_xml': ['hr_timesheet_sheet_view.xml', 'hr_analytic_timesheet_view.xml'],
+ 'update_xml': ['project_task_view.xml'],
  'demo_xml': [],
  'tests': [],
  'installable': True,
@@ -37,3 +37,5 @@
  'auto_install': False,
  'license': 'AGPL-3',
  'application': True}
+ #'icon': '/MODULE_NAME/static/src/images/XXX.png',
+ 
---
+++ timesheet_task/tmp_file_for_project_indicator.py
@@ -0,0 +1,29 @@
+def _progress_rate(self, cr, uid, ids, names, arg, context=None):
+    """As OpenERP SA made a query for this function field (perf. reason obviously), 
+    I must overide it all."""
+    result = {}.fromkeys(ids, 0.0)
+    progress = {}
+    if not ids:
+        return res
+    cr.execute('''SELECT project_id,
+                         sum(planned_hours) as sum_planned_hours,
+                         sum(total_hours) as sum_total_hours,
+                         sum(effective_hours) as sum_effective_hours,
+                         sum(remaining_hours) as remaining_hours,
+                         sum(deduced_hours) as deduced_hours
+                  FROM project_task
+                  WHERE  project_id in %s 
+                   AND state<>'cancelled'
+                  GROUP BY project_id''', (tuple(ids),))
+                 
+    res = cr.dictfetchall()
+    for stat in res:
+        project = self.browse(cr, uid, res['project_id'], context=context)
+        progr = (stat['sum_planned_hours'] and 
+                 round(100.0 * stat['sum_total_hours'] / stat['sum_planned_hours'], 2) or 0.0),
+        result[project.id] = {'planned_hours': stat['sum_planned_hours'],
+                              'effective_hours': stat['sum_effective_hours'],
+                              'total_hours': stat['sum_total_hours'],
+                              'progress_rate': progr,
+                              'deduced_hours': stat['sum_deduced_hours']}
+    return res
