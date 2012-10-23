d120efcbf03d6126131f15e5414920209a9a9de0
Guewen Baconnier @ Camptocamp <guewen.baconnier@camptocamp.com>
2012-10-23 17:13:34 +0200

    [MRG] 2 fixes on timesheet_task by Stéphane Bidoul @ Acsone:
       * fix bug when to_invoice is not set
       * add watcher on task_id so progress is recomputed when the task changes on a timesheet line
    (lp:c2c-addons/6.1  rev 115)

--- timesheet_task/project_task.py
+++ timesheet_task/project_task.py
@@ -30,7 +30,7 @@
 from osv import osv, fields
 
 TASK_WATCHERS = ['work_ids', 'remaining_hours', 'planned_hours']
-AA_WATCHERS = ['unit_amount','product_uom_id','account_id','to_invoice']
+AA_WATCHERS = ['unit_amount', 'product_uom_id', 'account_id', 'to_invoice', 'task_id']
 
 class ProjectTask(osv.osv):
     _inherit = "project.task"
@@ -158,13 +158,13 @@ class AccountAnalyticLine(osv.osv):
                 if new_task_id:
                     data = {'task_id': new_task_id,
                             'to_invoice': vals.get('to_invoice',
-                                               line.to_invoice and line.to_invoice.id or false),
+                                               line.to_invoice and line.to_invoice.id or False),
                             'unit_amount': vals.get('unit_amount', line.unit_amount)}
                     self._set_remaining_hours_create(cr, uid, data, context)
                 return ids
             if new_task_id:
                 hours = vals.get('unit_amount', line.unit_amount)
-                factor_id = vals.get('to_invoice', line.to_invoice.id)
+                factor_id = vals.get('to_invoice', line.to_invoice and line.to_invoice.id or False)
                 comp_hours = self._compute_hours_with_factor(cr, uid, hours, factor_id, context)
                 old_factor = line.to_invoice and line.to_invoice.id or False
                 old_comp_hours = self._compute_hours_with_factor(cr, uid, line.unit_amount,
