24c4b487b8478ab18cc945819b8f797f31c026bf
Laetitia Gangloff <laetitia.gangloff@acsone.eu>
2013-04-03 13:20:21 +0200

    in project_task used osv.Model instead of osv.osv. Update licence part.

--- timesheet_task/project_task.py
+++ timesheet_task/project_task.py
@@ -1,29 +1,21 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-# author Nicolas Bessi
-# Copyright Camptocamp 2012
+#    Author: Nicolas Bessi
+#    Copyright 2013 Camptocamp SA
 #
-# WARNING: This program as such is intended to be used by professional
-# programmers who take the whole responsability of assessing all potential
-# consequences resulting from its eventual inadequacies and bugs
-# End users who are looking for a ready-to-use solution with commercial
-# garantees and support are strongly adviced to contract a Free Software
-# Service Company
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
 #
-# This program is Free Software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 
@@ -32,7 +24,7 @@ from osv import osv, fields
 TASK_WATCHERS = ['work_ids', 'remaining_hours', 'planned_hours']
 TIMESHEET_WATCHERS = ['unit_amount', 'product_uom_id', 'account_id', 'to_invoice', 'task_id']
 
-class ProjectTask(osv.osv):
+class ProjectTask(osv.Model):
     _inherit = "project.task"
     _name = "project.task"
 
@@ -100,7 +92,7 @@ class ProjectTask(osv.osv):
                 ts_obj.write(cr, uid, [w.id for w in task.work_ids], {'account_id': account_id}, context=context)
         return res
 
-class HrAnalyticTimesheet(osv.osv):
+class HrAnalyticTimesheet(osv.Model):
     _inherit = "hr.analytic.timesheet"
     _name = "hr.analytic.timesheet"
 
@@ -124,7 +116,7 @@ class HrAnalyticTimesheet(osv.osv):
                     res['value']['to_invoice'] = p.to_invoice.id
         return res
 
-class AccountAnalyticLine(osv.osv):
+class AccountAnalyticLine(osv.Model):
     """We add task_id on AA and manage update of linked task indicators"""
     _inherit = "account.analytic.line"
     _name = "account.analytic.line"
