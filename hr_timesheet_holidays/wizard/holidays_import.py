3dd55ff984e71c22d0d9797852803d1795f426f3
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2013-05-13 10:23:34 +0200

    [IMP] use full path for import of osv

--- hr_timesheet_holidays/wizard/holidays_import.py
+++ hr_timesheet_holidays/wizard/holidays_import.py
@@ -19,7 +19,7 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-from osv import orm, fields, osv
+from openerp.osv import orm, fields, osv
 from tools.translate import _
 from datetime import datetime, timedelta
 
