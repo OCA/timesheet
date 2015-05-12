a9e81b7d4b4e62615af6704906f135e65005f03b
Alex Comba <alex.comba@agilebg.com>
2015-05-12 14:20:35 +0200

    Update code using by 'git checkout origin/7.0  -- hr_attendance_analysis'

--- hr_attendance_analysis/report/__init__.py
+++ hr_attendance_analysis/report/__init__.py
@@ -18,4 +18,4 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-import calendar_report
+from . import calendar_report
