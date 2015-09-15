6970badb492164bf33d2fb6916ff45f6b5c9cea0
Alex Comba <alex.comba@agilebg.com>
2015-09-15 17:00:23 +0200

    Update version according to https://github.com/OCA/maintainer-tools/blob/master/CONTRIBUTING.md#version-numbers

--- hr_attendance_analysis/__openerp__.py
+++ hr_attendance_analysis/__openerp__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 {
     'name': "HR - Attendance Analysis",
-    'version': '1.0',
+    'version': '8.0.1.0.0',
     'category': 'Generic Modules/Human Resources',
     'summary': "Dynamic reports based on employee's attendances and "
                "contract's calendar",
