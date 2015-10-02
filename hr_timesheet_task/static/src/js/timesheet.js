203477662dd57209a0e71a95e7c410b071e03f16
Adrien Peiffer (ACSONE) <adrien.peiffer@acsone.eu>
2015-10-02 10:10:37 +0200

    [REF][hr_timesheet_task] Move domain change out of init_add_account and avoid to call init method

--- hr_timesheet_task/static/src/js/timesheet.js
+++ hr_timesheet_task/static/src/js/timesheet.js
@@ -171,26 +171,7 @@ openerp.hr_timesheet_task = function(instance) {
             // when account_m2o loses focus, value can be changed, 
             // update task_m2o to show only tasks related to the selected project
             self.account_m2o.$input.focusout(function(){
-                var account_id = self.account_m2o.get_value();
-                if (account_id === false) { return; }
-                self.task_m2o.init(self.dfm, {
-                    attrs: {
-                        name: "task",
-                        type: "many2one",
-                        domain: [
-                            // show only tasks linked to the selected project
-                            ['project_id.analytic_account_id','=',account_id],
-                            // ignore tasks already in the timesheet
-                            ['id', 'not in', _.pluck(self.accounts, "task")],
-                        ],
-                        context: {
-                            'account_id': account_id,
-                        },
-                    },
-                });
-                // reset value previously selected
-                self.task_m2o.set_value(false);
-                self.task_m2o.render_value();
+                self.onchange_account_id()
             });
 
             self.$(".oe_timesheet_weekly_add_row button").click(function() {
@@ -212,6 +193,20 @@ openerp.hr_timesheet_task = function(instance) {
                     self.set({"sheets": ops});
                 });
             });
+        },
+        onchange_account_id: function() {
+            var self = this
+            var account_id = self.account_m2o.get_value();
+            if (account_id === false) { return; }
+            self.task_m2o.node.attrs.domain = [
+               // show only tasks linked to the selected project
+               ['project_id.analytic_account_id','=',account_id],
+               // ignore tasks already in the timesheet
+               ['id', 'not in', _.pluck(self.accounts, "task")],
+            ]
+            self.task_m2o.node.attrs.context = {'account_id': account_id};
+            self.task_m2o.set_value(false);
+            self.task_m2o.render_value();
         },
         get_box: function(account, day_count) {
             return this.$('[data-account-task="' + account.account_task + '"][data-day-count="' + day_count + '"]');
