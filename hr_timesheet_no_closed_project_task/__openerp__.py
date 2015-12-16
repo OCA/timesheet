d2f31f08ef25331bfa54f428304365a37422ac10
Adrien Peiffer (ACSONE) <adrien.peiffer@acsone.eu>
2015-12-16 16:33:45 +0100

    Some cosmetic changes

--- hr_timesheet_no_closed_project_task/__openerp__.py
+++ hr_timesheet_no_closed_project_task/__openerp__.py
@@ -1,37 +1,17 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#     This file is part of hr_timesheet_no_closed_project_task,
-#     an Odoo module.
-#
-#     Copyright (c) 2015 ACSONE SA/NV (<http://acsone.eu>)
-#
-#     hr_timesheet_invoice_hide_to_invoice is free software:
-#     you can redistribute it and/or modify it under the terms of the GNU
-#     Affero General Public License as published by the Free Software
-#     Foundation,either version 3 of the License, or (at your option) any
-#     later version.
-#
-#     hr_timesheet_invoice_hide_to_invoice is distributed
-#     in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
-#     even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-#     PURPOSE.  See the GNU Affero General Public License for more details.
-#
-#     You should have received a copy of the GNU Affero General Public License
-#     along with hr_timesheet_no_closed_project_task.
-#     If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright 2015 ACSONE SA/NV (<http://acsone.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "HR Timesheet No Closed Project-Task",
     'summary': """
         Prevent to select closed project or task on timesheet line""",
     "version": "8.0.1.0.0",
-    "author": "ACSONE SA/NV",
-    "maintainer": "ACSONE SA/NV",
+    "author": "ACSONE SA/NV,"
+              "Odoo Community Association (OCA)",
     "website": "http://www.acsone.eu",
     "category": "Human Resources",
+    "license": "AGPL-3",
     "depends": [
         "hr_timesheet_task",
         "hr_timesheet_invoice",
@@ -45,6 +25,4 @@
         'views/project_task_view.xml',
         'views/hr_timesheet_no_closed_project_task.xml',
     ],
-    "license": "AGPL-3",
-    "installable": True,
 }
