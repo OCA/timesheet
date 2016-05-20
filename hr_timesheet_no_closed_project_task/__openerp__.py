9002519b286e975ab33dbdd86199ae885a176283
Adrien Peiffer (ACSONE) <adrien.peiffer@acsone.eu>
2016-05-20 15:08:57 +0200

    [FIX][hr_timesheet_no_closed_project_task] Remove line in data key in the manifest.

--- hr_timesheet_no_closed_project_task/__openerp__.py
+++ hr_timesheet_no_closed_project_task/__openerp__.py
@@ -20,7 +20,6 @@
     ],
     "data": [
         'security/ir.model.access.csv',
-        'security/hr_timesheet_no_closed_project_task_security.xml',
         'views/hr_analytic_timesheet_view.xml',
         'views/hr_timesheet_sheet_view.xml',
         'views/project_task_view.xml',
