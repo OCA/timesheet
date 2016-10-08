1cae59e0eb6ce9a5b94bcd6e181ab62270578c7e
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2016-10-08 11:23:40 +0200

    prefix versions with 9.0

--- hr_timesheet_sheet_week_start_day/__manifest__.py
+++ hr_timesheet_sheet_week_start_day/__manifest__.py
@@ -6,7 +6,7 @@
     "name": "Hr Timesheet Sheet Week Start Day",
     "summary": "Allows to define the week start date for Timesheets at "
                "company level",
-    "version": "8.0.1.0.0",
+    "version": "9.0.1.0.0",
     "author": "Eficent Business and IT Consulting Services, S.L., "
               "Odoo Community Association (OCA)",
     "website": "http://www.eficent.com",
