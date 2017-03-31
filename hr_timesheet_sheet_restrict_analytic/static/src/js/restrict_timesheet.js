9b201175b36bde854c7be9b680b8863f294d0919
Jordi Ballester <jordi.ballester@eficent.com>
2017-03-31 18:59:55 +0200

    [FIX] travis

--- hr_timesheet_sheet_restrict_analytic/static/src/js/restrict_timesheet.js
+++ hr_timesheet_sheet_restrict_analytic/static/src/js/restrict_timesheet.js
@@ -1,6 +1,6 @@
 odoo.define('hr_timesheet_restrict_analytic.restrict', function (require) {
 'use strict';
-
+    var core = require('web.core');
     var hr_timesheet_sheet = core.form_custom_registry.get('weekly_timesheet');
     hr_timesheet_sheet.include({
          init_add_account: function() {
