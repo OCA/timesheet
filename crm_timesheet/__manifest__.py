539bcf82896f201d45131bd18a8268a280ddd961
Pedro M. Baeza <pedro.baeza@gmail.com>
2017-08-17 17:13:08 +0200

    [IMP] crm_timesheet: Better layout

--- crm_timesheet/__manifest__.py
+++ crm_timesheet/__manifest__.py
@@ -7,7 +7,7 @@
 {
     'name': "CRM Timesheet",
     'category': 'Customer Relationship Management',
-    'version': '10.0.1.0.0',
+    'version': '10.0.1.1.0',
     'depends': [
         'crm',
         'hr_timesheet'
