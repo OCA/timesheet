4fc9a71780dd2a0110ace1d39d79245431e2e2d4
aheficent <ahenriquez@eficent.com>
2017-12-14 18:28:31 +0100

    [IMP] Adapt to date range model and improve coverage

--- hr_timesheet_sheet_period/tests/__init__.py
+++ hr_timesheet_sheet_period/tests/__init__.py
@@ -1,8 +1,3 @@
 # -*- coding: utf-8 -*-
-# Copyright 2016-17 Eficent Business and IT Consulting Services S.L.
-#   (http://www.eficent.com)
-# Copyright 2016-17 Serpent Consulting Services Pvt. Ltd.
-#   (<http://www.serpentcs.com>)
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from . import test_hr_timesheet_sheet_period
