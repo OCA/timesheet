978c164acca7c5690516efc8842ca7a1d7a6d9ef
Benjamin Willig <benjamin.willig@acsone.eu>
2018-02-21 10:02:00 +0100

    [11.0][MIG] hr_timesheet_task_required migration

--- hr_timesheet_task_required/__manifest__.py
+++ hr_timesheet_task_required/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2016-2017 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
@@ -6,16 +5,14 @@
     'name': 'Hr Timesheet Task Required',
     'summary': """
         Set task on timesheet as a mandatory field""",
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'license': 'AGPL-3',
     'author': 'ACSONE SA/NV, Odoo Community Association (OCA)',
     'website': 'https://github.com/OCA/hr-timesheet',
     'depends': [
-        'hr_timesheet_task',
+        'hr_timesheet',
     ],
     'data': [
         'views/account_analytic_line.xml',
-        'views/assets.xml',
-        'views/hr_timesheet_sheet.xml',
     ],
 }
