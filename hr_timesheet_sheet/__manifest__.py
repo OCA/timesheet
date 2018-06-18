22dea6e42b701a12a381a83214307c4d91915f68
Miquel Raïch <miquel.raich@eficent.com>
2018-06-18 12:36:53 +0200

    [11.0][MIG] hr_timesheet_sheet (#125)
    * [10.0] hr_timesheet_sheet
    
    * [11.0][MIG] hr_timesheet_sheet
    
    * [REMOVE] hr_timesheet.sheet.account
    
    * [REMOVE] 'new' state
    
    * [ADD] Tests
    
    * [UPD] Adapt to multicompany
    
    * [ADD] Add more tests (include multicompany tests)
    
    * [FIX] project_task_stage_allow_timesheet: show error message only if task
    
    * [ADD] Migration scripts to v11

---
+++ hr_timesheet_sheet/__manifest__.py
@@ -0,0 +1,27 @@
+# Copyright 2018 Eficent
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'HR Timesheet Sheet',
+    'version': '11.0.1.0.0',
+    'category': 'Human Resources',
+    'sequence': 80,
+    'summary': 'Timesheet Sheets, Activities',
+    'license': 'AGPL-3',
+    'author': 'Eficent, Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/hr-timesheet',
+    'depends': [
+        'hr_timesheet',
+        'web_widget_x2many_2d_matrix',
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'security/hr_timesheet_sheet_security.xml',
+        'data/hr_timesheet_sheet_data.xml',
+        'views/hr_timesheet_sheet_views.xml',
+        'views/hr_department_views.xml',
+        'views/res_config_settings_views.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
