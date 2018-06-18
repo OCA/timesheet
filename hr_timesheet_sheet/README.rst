ece9ee7f3eea98ecb1c7380949c33223ef6892bf
Miquel Raïch <miquel.raich@eficent.com>
2018-06-18 12:36:53 +0200

    [11.0][MIG] hr_timesheet_sheet (#125)
    * [10.0] hr_timesheet_sheet
    
    * [11.0][MIG] hr_timesheet_sheet
    
    * [REMOVE] hr_timesheet.sheet.account
    
    * [REMOVE] 'new' state
    
    * [ADD] Tests
    
    * [UPD] Adapt to multicompany
    
    * [ADD] Add more tests (include multicompany tests)
    
    * [FIX] project_task_stage_allow_timesheet: show error message only if task
    
    * [ADD] Migration scripts to v11

---
+++ hr_timesheet_sheet/README.rst
@@ -0,0 +1,80 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.png
+   :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   :alt: License: AGPL-3
+
+==================
+HR Timesheet Sheet
+==================
+
+This module supplies a new screen enabling you to manage your work encoding (timesheet) by period.
+Timesheet entries are made by employees each day. At the end of the defined period,
+employees validate their sheet and the manager must then approve his team's entries.
+Periods are defined in the company forms and you can set them to run monthly, weekly or daily.
+
+Negative hours are not admitted.
+
+Installation
+============
+
+This module relies on:
+
+* The OCA module '2D matrix for x2many fields', and can be downloaded from
+  Github: https://github.com/OCA/web/tree/11.0/web_widget_x2many_2d_matrix
+
+
+Configuration
+=============
+
+If you want other default ranges different from weekly, you need to go:
+
+* In the menu `Configuration` -> `Settings` -> **Timesheet Options**,
+  and select in **Timesheet Sheet Range** the default range you want.
+
+
+.. image:: https://odoo-community.org/website/image/ir.attachment/5784_f2813bd/datas
+   :alt: Try me on Runbot
+   :target: https://runbot.odoo-community.org/runbot/117/11.0
+
+Known issues / Roadmap
+======================
+
+* When you open the `Summary` or `Details` tab, a save should be performed
+  to ensure the data shown is correct. This perhaps could be achieved by including
+  a .js file that does that.
+
+Bug Tracker
+===========
+
+Bugs are tracked on `GitHub Issues
+<https://github.com/OCA/hr-timesheet/issues>`_. In case of trouble, please
+check there if your issue has already been reported. If you spotted it first,
+help us smash it by providing detailed and welcomed feedback.
+
+Credits
+=======
+
+Images
+------
+
+* Odoo Community Association: `Icon <https://github.com/OCA/maintainer-tools/blob/master/template/module/static/description/icon.svg>`_.
+
+Contributors
+------------
+
+* Miquel Raïch <miquel.raich@eficent.com>
+
+
+Maintainer
+----------
+
+.. image:: https://odoo-community.org/logo.png
+   :alt: Odoo Community Association
+   :target: https://odoo-community.org
+
+This module is maintained by the OCA.
+
+OCA, or the Odoo Community Association, is a nonprofit organization whose
+mission is to support the collaborative development of Odoo features and
+promote its widespread use.
+
+To contribute to this module, please visit https://odoo-community.org.
---
+++ hr_timesheet_sheet/models/account_analytic_line.py
@@ -0,0 +1,92 @@
+# Copyright 2018 Eficent Business and IT Consulting Services, S.L.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError
+
+
+class AccountAnalyticLine(models.Model):
+    _inherit = 'account.analytic.line'
+
+    sheet_id_computed = fields.Many2one(
+        comodel_name='hr_timesheet.sheet',
+        string='Sheet',
+        compute='_compute_sheet',
+        index=True,
+        ondelete='cascade',
+        search='_search_sheet'
+    )
+    sheet_id = fields.Many2one(
+        comodel_name='hr_timesheet.sheet',
+        string='Sheet',
+        compute='_compute_sheet',
+        store=True,
+    )
+
+    @api.depends('date', 'user_id', 'project_id', 'task_id', 'company_id',
+                 'sheet_id.date_start', 'sheet_id.date_end',
+                 'sheet_id.employee_id', 'sheet_id.company_id')
+    def _compute_sheet(self):
+        """Links the timesheet line to the corresponding sheet"""
+        for timesheet in self:
+            if timesheet.sheet_id or not timesheet.project_id:
+                continue
+            sheets = self.env['hr_timesheet.sheet'].search(
+                [('date_end', '>=', timesheet.date),
+                 ('date_start', '<=', timesheet.date),
+                 ('employee_id.user_id.id', '=', timesheet.user_id.id),
+                 ('company_id', 'in', [timesheet.company_id.id, False]),
+                 ('state', '=', 'draft'),
+                 ])
+            if sheets:
+                timesheet.sheet_id_computed = sheets[0]
+                timesheet.sheet_id = sheets[0]
+
+    def _search_sheet(self, operator, value):
+        assert operator == 'in'
+        ids = []
+        for ts in self.env['hr_timesheet.sheet'].browse(value):
+            self._cr.execute("""
+                    SELECT l.id
+                        FROM account_analytic_line l
+                    WHERE %(date_end)s >= l.date
+                        AND %(date_start)s <= l.date
+                        AND %(user_id)s = l.user_id
+                        AND %(company_id)s = l.company_id
+                    GROUP BY l.id""", {'date_start': ts.date_start,
+                                       'date_end': ts.date_end,
+                                       'user_id': ts.employee_id.user_id.id,
+                                       'company_id': ts.company_id.id,
+                                       })
+            ids.extend([row[0] for row in self._cr.fetchall()])
+        return [('id', 'in', ids)]
+
+    @api.multi
+    def write(self, values):
+        self._check_state()
+        return super(AccountAnalyticLine, self).write(values)
+
+    @api.multi
+    def unlink(self):
+        self._check_state()
+        return super(AccountAnalyticLine, self).unlink()
+
+    def _check_state(self):
+        for line in self:
+            if line.sheet_id and line.sheet_id.state != 'draft':
+                raise UserError(
+                    _('You cannot modify an entry in a confirmed '
+                      'timesheet sheet.'))
+        return True
+
+    @api.multi
+    def merge_timesheets(self):
+        unit_amount = sum(
+            [t.unit_amount for t in self])
+        amount = sum([t.amount for t in self])
+        self[0].write({
+            'unit_amount': unit_amount,
+            'amount': amount,
+        })
+        self[1:].unlink()
+        return self[0]
