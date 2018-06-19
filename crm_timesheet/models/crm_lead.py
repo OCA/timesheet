d1a220eea58fdd485d661ba0ef3f6e9682acc843
cubells <vicent@vcubells.net>
2018-06-19 13:02:55 +0200

    [MIG] crm_timesheet: Migration to 11.0

--- crm_timesheet/models/crm_lead.py
+++ crm_timesheet/models/crm_lead.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2015 Antonio Espinosa <antonio.espinosa@tecnativa.com>
 # Copyright 2015 Javier Iniesta <javieria@antiun.com>
 # Copyright 2017 David Vidal <david.vidal@tecnativa.com>
@@ -11,9 +10,11 @@ class CrmLead(models.Model):
     _inherit = 'crm.lead'
 
     project_id = fields.Many2one(
-        comodel_name='project.project', string="Project",
+        comodel_name='project.project',
+        string="Project",
     )
     timesheet_ids = fields.One2many(
-        comodel_name='account.analytic.line', inverse_name='lead_id',
+        comodel_name='account.analytic.line',
+        inverse_name='lead_id',
         string="Timesheet",
     )
