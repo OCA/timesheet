6abccf0a3e18a6717a1c0765a40d584d850f9cd8
cubells <vicent@vcubells.net>
2018-06-19 13:02:55 +0200

    [MIG] crm_timesheet: Migration to 11.0

--- crm_timesheet/models/account_analytic_line.py
+++ crm_timesheet/models/account_analytic_line.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2015 Antonio Espinosa <antonio.espinosa@tecnativa.com>
 # Copyright 2015 Javier Iniesta <javieria@antiun.com>
 # Copyright 2017 David Vidal <david.vidal@tecnativa.com>
@@ -10,8 +9,10 @@ from odoo import api, fields, models
 class AccountAnalyticLine(models.Model):
     _inherit = 'account.analytic.line'
 
-    lead_id = fields.Many2one(comodel_name='crm.lead',
-                              string='Lead/Opportunity')
+    lead_id = fields.Many2one(
+        comodel_name='crm.lead',
+        string='Lead/Opportunity',
+    )
 
     @api.onchange('lead_id')
     def _onchange_lead_id(self):
