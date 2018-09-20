a7cd73f69685fd6755281019de80f91041714239
Andrea <a.stirpe@onestein.nl>
2018-09-20 16:30:03 +0200

    Code review

--- hr_timesheet_sheet/models/hr_timesheet_sheet.py
+++ hr_timesheet_sheet/models/hr_timesheet_sheet.py
@@ -126,14 +126,13 @@ class Sheet(models.Model):
     )
     total_time = fields.Float(
         compute='_compute_total_time',
-        store=True
+        store=True,
     )
 
     @api.depends('timesheet_ids.unit_amount')
     def _compute_total_time(self):
         for sheet in self:
-            sheet.total_time = sum(
-                line.unit_amount for line in sheet.timesheet_ids)
+            sheet.total_time = sum(sheet.mapped('timesheet_ids.unit_amount'))
 
     @api.constrains('date_start', 'date_end')
     def _check_start_end_dates(self):
