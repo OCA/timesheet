3b6162a7fa4e574495f1f08dec5b029c4f6a12a8
Andrea <a.stirpe@onestein.nl>
2018-09-20 16:03:16 +0200

    [IMP] hr_timesheet_sheet ux

--- hr_timesheet_sheet/__manifest__.py
+++ hr_timesheet_sheet/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'HR Timesheet Sheet',
-    'version': '11.0.1.1.1',
+    'version': '11.0.1.2.0',
     'category': 'Human Resources',
     'sequence': 80,
     'summary': 'Timesheet Sheets, Activities',
