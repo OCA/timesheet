c1a3447451659e9671c935cc3df84224315e5a78
Andrea Stirpe <a.stirpe@onestein.nl>
2018-10-01 11:49:59 +0200

    [FIX] hr_timesheet_sheet: "Refuse" button name (#134)

--- hr_timesheet_sheet/__manifest__.py
+++ hr_timesheet_sheet/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'HR Timesheet Sheet',
-    'version': '11.0.1.2.0',
+    'version': '11.0.1.2.1',
     'category': 'Human Resources',
     'sequence': 80,
     'summary': 'Timesheet Sheets, Activities',
