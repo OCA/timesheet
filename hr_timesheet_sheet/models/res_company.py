14184c69a729360b60b2e81a235717e519b3650d
Lois Rilo <lois.rilo@eficent.com>
2018-10-17 17:15:21 +0200

    [11.0][IMP] hr_timesheet_sheet: merge hr_timesheet_sheet_week_start_day into this module, which adds a configuration to select the week start day.

--- hr_timesheet_sheet/models/res_company.py
+++ hr_timesheet_sheet/models/res_company.py
@@ -4,6 +4,16 @@
 from odoo import fields, models
 from dateutil.rrule import (MONTHLY, WEEKLY, DAILY)
 
+_WEEKDAYS = [
+    ('0', 'Monday'),
+    ('1', 'Tuesday'),
+    ('2', 'Wednesday'),
+    ('3', 'Thursday'),
+    ('4', 'Friday'),
+    ('5', 'Saturday'),
+    ('6', 'Sunday')
+]
+
 
 class ResCompany(models.Model):
     _inherit = 'res.company'
@@ -15,3 +25,8 @@ class ResCompany(models.Model):
         string='Timesheet Sheet Range',
         default=WEEKLY,
         help="The range of your Timesheet Sheet.")
+
+    timesheet_week_start = fields.Selection(
+        selection=_WEEKDAYS,
+        string='Week start day',
+        default='0')
