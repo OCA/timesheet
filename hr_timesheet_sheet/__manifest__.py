283d3af501779df0aa1af379d7d11bccf553ed1f
Alexey Pelykh <alexey.pelykh@gmail.com>
2018-11-09 18:36:01 +0200

    [MIG] hr_timesheet_sheet: Migration to 12.0

--- hr_timesheet_sheet/__manifest__.py
+++ hr_timesheet_sheet/__manifest__.py
@@ -3,25 +3,28 @@
 
 {
     'name': 'HR Timesheet Sheet',
-    'version': '11.0.1.2.1',
+    'version': '12.0.1.0.0',
     'category': 'Human Resources',
     'sequence': 80,
     'summary': 'Timesheet Sheets, Activities',
     'license': 'AGPL-3',
-    'author': 'Eficent, Odoo Community Association (OCA)',
+    'author': (
+        'Eficent, '
+        'Odoo Community Association (OCA)'
+    ),
     'website': 'https://github.com/OCA/hr-timesheet',
+    'installable': True,
+    'auto_install': False,
     'depends': [
         'hr_timesheet',
         'web_widget_x2many_2d_matrix',
     ],
     'data': [
+        'data/hr_timesheet_sheet_data.xml',
         'security/ir.model.access.csv',
         'security/hr_timesheet_sheet_security.xml',
-        'data/hr_timesheet_sheet_data.xml',
         'views/hr_timesheet_sheet_views.xml',
         'views/hr_department_views.xml',
         'views/res_config_settings_views.xml',
     ],
-    'installable': True,
-    'auto_install': False,
 }
