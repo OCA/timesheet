7aa424c5ddf33530d4f761d6d6aa444c4769a3aa
Beñat Jimenez <benat.jimenez@eficent.com>
2018-11-26 13:02:55 +0100

    [MIG] Migration of module hr_employee_product to version 12

--- hr_employee_product/__init__.py
+++ hr_employee_product/__init__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
-# Copyright 2015-17 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl-3.0).
+
 from . import model
