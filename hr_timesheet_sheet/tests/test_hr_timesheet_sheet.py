4d1b0158945c178965389916a74f7ef0224e75bd
Pedro M. Baeza <pedro.baeza@gmail.com>
2018-12-09 01:13:28 +0100

    [FIX] hr_timesheet_sheet: Don't fail tests on Sunday
    If you run tests on Sunday, test_4 was not prepared for it, as next day is Monday,
    which belongs to other week, and thus not included in same timesheet. With this,
    we cover that case, decreasing one day instead of adding it.

--- hr_timesheet_sheet/tests/test_hr_timesheet_sheet.py
+++ hr_timesheet_sheet/tests/test_hr_timesheet_sheet.py
@@ -224,8 +224,11 @@ class TestHrTimesheetSheet(TransactionCase):
             'project_id': self.project_1.id,
             'employee_id': self.employee.id,
         })
-        timesheet_3.date = fields.Date.to_string(
-            fields.Date.from_string(timesheet_3.date) + relativedelta(days=1))
+        dt = fields.Date.from_string(timesheet_3.date)
+        # With this we assure to be in the same week but different day
+        # (for covering today = sunday)
+        days = -1 if dt.weekday() == 6 else 1
+        timesheet_3.date = fields.Date.to_string(dt + relativedelta(days=days))
 
         sheet = self.sheet_model.sudo(self.user).create({
             'employee_id': self.employee.id,
