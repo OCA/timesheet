7fdee0014319e8238bb1ee5adb5aeb4f2b409204
Alexey Pelykh <alexey.pelykh@gmail.com>
2018-12-10 19:20:31 +0200

    [FIX] hr_timesheet_task_required: 'position' instead of 'poistion'

--- hr_timesheet_task_required/README.rst
+++ hr_timesheet_task_required/README.rst
@@ -38,6 +38,7 @@ Contributors
 * Adrien Peiffer <adrien.peiffer@acsone.eu>
 * Laurent Mignon <laurent.mignon@acsone.eu>
 * Benjamin Willig <benjamin.willig@acsone.eu>
+* Alexey Pelykh <alexey.pelykh@brainbeanapps.com>
 
 Maintainer
 ----------
--- hr_timesheet_task_required/__manifest__.py
+++ hr_timesheet_task_required/__manifest__.py
@@ -6,7 +6,7 @@
     'name': 'Hr Timesheet Task Required',
     'summary': """
         Set task on timesheet as a mandatory field""",
-    'version': '10.0.1.0.0',
+    'version': '10.0.1.0.1',
     'license': 'AGPL-3',
     'author': 'ACSONE SA/NV, Odoo Community Association (OCA)',
     'website': 'https://github.com/OCA/hr-timesheet',
