f4671efdb009abb595efc0ba1a077c26ebe89c72
Adrià Gil Sorribes <adria.gil@eficent.com>
2018-12-18 12:11:17 +0100

    Migrate module hr_timesheet_sheet_period to v12

--- hr_timesheet_sheet_period/__manifest__.py
+++ hr_timesheet_sheet_period/__manifest__.py
@@ -1,16 +1,15 @@
-# -*- coding: utf-8 -*-
 # Copyright 2016-17 Eficent Business and IT Consulting Services S.L.
 # Copyright 2016-17 Serpent Consulting Services Pvt. Ltd.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     'name': "HR Timesheet Sheet based on Payroll Period",
-    'version': '10.0.1.0.0',
+    'version': '12.0.1.0.0',
     'category': 'Human Resources',
     "author": "Eficent Business and IT Consulting Services S.L., "
               "Serpent Consulting Services Pvt. Ltd., "
               "Odoo Community Association (OCA)",
-    'website': 'http://www.eficent.com',
+    'website': 'https://github.com/OCA/hr-timesheet',
     'license': 'AGPL-3',
     "depends": ['hr_period', 'hr_timesheet_sheet'],
     "data": [
