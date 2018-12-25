1726a394f5afcb287651feef85571e5d680a5e0e
Alexey Pelykh <alexey.pelykh@gmail.com>
2018-12-25 20:35:56 +0200

    [ADD] hr_utilization_report: generate Utilization Report from Task Logs

---
+++ hr_utilization_report/__manifest__.py
@@ -0,0 +1,26 @@
+# Copyright 2018 Brainbean Apps (https://brainbeanapps.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Task Logs Utilization Report',
+    'version': '12.0.1.0.0',
+    'category': 'Human Resources',
+    'website': 'https://github.com/OCA/hr-timesheet',
+    'author':
+        'Brainbean Apps, '
+        'Odoo Community Association (OCA)',
+    'license': 'AGPL-3',
+    'installable': True,
+    'application': False,
+    'summary': 'Generate Utilization Report from Task Logs',
+    'depends': [
+        'hr_timesheet',
+        'report_xlsx',
+    ],
+    'data': [
+        'views/hr_department.xml',
+        'views/hr_employee.xml',
+        'report/hr_utilization_report.xml',
+        'wizards/hr_utilization_report_wizard.xml',
+    ],
+}
