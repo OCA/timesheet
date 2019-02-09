b09bdf973f4835f586228c6026232e9cfee8629a
Alexey Pelykh <alexey.pelykh@gmail.com>
2019-02-09 07:28:37 +0200

    [IMP] hr_timesheet_task_required: configurable

--- hr_timesheet_task_required/__manifest__.py
+++ hr_timesheet_task_required/__manifest__.py
@@ -1,5 +1,6 @@
 # Copyright 2016-2017 ACSONE SA/NV
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# Copyright 2018-2019 Brainbean Apps (https://brainbeanapps.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Hr Timesheet Task Required',
@@ -9,6 +10,7 @@
     'license': 'AGPL-3',
     'author':
         'ACSONE SA/NV, '
+        'Brainbean Apps, '
         'Odoo Community Association (OCA)',
     'website': 'https://github.com/OCA/hr-timesheet',
     'depends': [
@@ -16,5 +18,7 @@
     ],
     'data': [
         'views/account_analytic_line.xml',
+        'views/project_project.xml',
+        'views/res_config_settings.xml',
     ],
 }
