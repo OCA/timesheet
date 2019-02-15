0b91b59bf420efe64e2ce1f5c641b63163b6e5db
Alexey Pelykh <alexey.pelykh@gmail.com>
2019-02-15 18:22:03 +0200

    [IMP] hr_timesheet_sheet: texts & formatting

--- hr_timesheet_sheet/__manifest__.py
+++ hr_timesheet_sheet/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     'name': 'HR Timesheet Sheet',
-    'version': '12.0.1.0.3',
+    'version': '12.0.1.0.4',
     'category': 'Human Resources',
     'sequence': 80,
     'summary': 'Timesheet Sheets, Activities',
