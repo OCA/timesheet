c214546f1ea75f0aa291783609584f068b49f3a3
Andrea <a.stirpe@onestein.nl>
2019-02-26 10:24:39 +0100

    [FIX] hr_timesheet_sheet: sheet_id when modifying the timesheet

--- hr_timesheet_sheet/__manifest__.py
+++ hr_timesheet_sheet/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     'name': 'HR Timesheet Sheet',
-    'version': '12.0.1.0.4',
+    'version': '12.0.1.0.5',
     'category': 'Human Resources',
     'sequence': 80,
     'summary': 'Timesheet Sheets, Activities',
