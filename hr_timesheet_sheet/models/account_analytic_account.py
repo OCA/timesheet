5b48b6f749c6ba2de93edb1a92f3500ec4f60725
Andrea <a.stirpe@onestein.nl>
2019-03-25 10:19:42 +0100

    [ADD] Constraint company_id in timesheet sheet line

---
+++ hr_timesheet_sheet/models/account_analytic_account.py
@@ -0,0 +1,21 @@
+# Copyright 2019 Onestein (<https://www.onestein.eu>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import api, models, _
+from odoo.exceptions import ValidationError
+
+
+class AccountAnalyticAccount(models.Model):
+    _inherit = 'account.analytic.account'
+
+    @api.constrains('company_id')
+    def _check_timesheet_sheet_company_id(self):
+        for rec in self.sudo():
+            sheets = rec.line_ids.mapped('sheet_id').filtered(
+                lambda s: s.company_id and s.company_id != rec.company_id)
+            if sheets:
+                raise ValidationError(_(
+                    'You cannot change the company, as this %s (%s) '
+                    'is assigned to %s (%s).'
+                ) % (rec._name, rec.display_name,
+                     sheets[0]._name, sheets[0].display_name))
