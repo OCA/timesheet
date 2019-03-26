3b7e07d7ad25d08e5b74afb926a7dac0da48869c
Alexey Pelykh <alexey.pelykh@gmail.com>
2019-03-26 18:24:19 +0200

    [FIX] hr_timesheet_sheet: sheet name

--- hr_timesheet_sheet/__manifest__.py
+++ hr_timesheet_sheet/__manifest__.py
@@ -5,7 +5,7 @@
 
 {
     'name': 'HR Timesheet Sheet',
-    'version': '12.0.1.1.0',
+    'version': '12.0.1.1.1',
     'category': 'Human Resources',
     'sequence': 80,
     'summary': 'Timesheet Sheets, Activities',
