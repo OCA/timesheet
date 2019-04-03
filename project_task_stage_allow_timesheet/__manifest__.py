b1bf2af9608fdab6f90ff5cf13bc2dd51cc170ce
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2019-04-03 13:50:55 -0400

    [12.0] Migrate project_task_stage_allow_timesheet

--- project_task_stage_allow_timesheet/__manifest__.py
+++ project_task_stage_allow_timesheet/__manifest__.py
@@ -5,7 +5,7 @@
     'name': 'Project Task Stage Allow Timesheet',
     'summary': """
         Allows to tell that a task stage is opened for timesheets.""",
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'license': 'AGPL-3',
     'author': 'Odoo Community Association (OCA), ACSONE SA/NV',
     'website': 'https://github.com/OCA/hr-timesheet',
