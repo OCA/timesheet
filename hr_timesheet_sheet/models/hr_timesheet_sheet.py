04df2593c8c56d6c648214276e28c511987acc41
Andrea Stirpe <a.stirpe@onestein.nl>
2019-04-03 10:01:45 +0200

    [12.0][FIX] hr_timesheet_sheet: subscribe user when confirm (#227)

--- hr_timesheet_sheet/models/hr_timesheet_sheet.py
+++ hr_timesheet_sheet/models/hr_timesheet_sheet.py
@@ -392,10 +392,10 @@ class Sheet(models.Model):
         return super().unlink()
 
     def _timesheet_subscribe_users(self):
-        for sheet in self:
-            if sheet.employee_id.parent_id.user_id:
-                self.message_subscribe_users(
-                    user_ids=[sheet.employee_id.parent_id.user_id.id])
+        for sheet in self.sudo():
+            manager = sheet.employee_id.parent_id.user_id.partner_id
+            if manager:
+                self.message_subscribe(partner_ids=manager.ids)
 
     @api.multi
     def action_timesheet_draft(self):
