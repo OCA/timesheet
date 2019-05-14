f800bfa2f3f7419fda402f6ae663d7c01f607269
Alexey Pelykh <alexey.pelykh@gmail.com>
2019-05-14 21:56:03 +0300

    [FIX] project_task_stage_allow_timesheet: check if stage is set

--- project_task_stage_allow_timesheet/models/project_task_type.py
+++ project_task_stage_allow_timesheet/models/project_task_type.py
@@ -1,5 +1,5 @@
 # Copyright 2018 ACSONE SA/NV
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo import fields, models
 
