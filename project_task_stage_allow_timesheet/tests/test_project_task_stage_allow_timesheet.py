485021b21ae70f24929b0ccbd467e9154fe5e680
Alexey Pelykh <alexey.pelykh@gmail.com>
2019-05-14 21:56:03 +0300

    [FIX] project_task_stage_allow_timesheet: check if stage is set

--- project_task_stage_allow_timesheet/tests/test_project_task_stage_allow_timesheet.py
+++ project_task_stage_allow_timesheet/tests/test_project_task_stage_allow_timesheet.py
@@ -1,5 +1,6 @@
 # Copyright 2018 ACSONE SA/NV
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# Copyright 2019 Brainbean Apps (https://brainbeanapps.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo.tests.common import SavepointCase
 from odoo.exceptions import ValidationError
@@ -18,17 +19,15 @@ class TestProjectTaskStageAllowTimesheet(SavepointCase):
         cls.stage_new = cls.env.ref('project.project_stage_data_0')
 
     def test_01_stage_allow_timesheet(self):
-        self.stage_new.allow_timesheet = False
         self.task_1.stage_id = self.stage_new
 
         values = {
-            'name': "test",
+            'name': 'test',
             'project_id': self.project_1.id,
             'task_id': self.task_1.id,
         }
+        self.AnalyticLine.create(values)
 
+        self.stage_new.allow_timesheet = False
         with self.assertRaises(ValidationError), self.env.cr.savepoint():
             self.AnalyticLine.create(values)
-
-        self.stage_new.allow_timesheet = True
-        self.AnalyticLine.create(values)
