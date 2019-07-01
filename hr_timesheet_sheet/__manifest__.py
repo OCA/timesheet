9ed1b29b26e1622d2d752a17080fa62ef04fb020
Alexey Pelykh <alexey.pelykh@gmail.com>
2019-07-01 17:46:59 +0300

    [IMP] hr_timesheet_sheet: more hooks

--- hr_timesheet_sheet/__manifest__.py
+++ hr_timesheet_sheet/__manifest__.py
@@ -1,11 +1,11 @@
 # Copyright 2018 Eficent
 # Copyright 2018-2019 Brainbean Apps (https://brainbeanapps.com)
 # Copyright 2018-2019 Onestein (<https://www.onestein.eu>)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
     'name': 'HR Timesheet Sheet',
-    'version': '12.0.2.0.0',
+    'version': '12.0.2.1.0',
     'category': 'Human Resources',
     'sequence': 80,
     'summary': 'Timesheet Sheets, Activities',
@@ -13,9 +13,10 @@
     'author': (
         'Eficent, '
         'Onestein, '
+        'Brainbean Apps, '
         'Odoo Community Association (OCA)'
     ),
-    'website': 'https://github.com/OCA/hr-timesheet',
+    'website': 'https://github.com/OCA/timesheet',
     'installable': True,
     'auto_install': False,
     'depends': [
@@ -28,6 +29,8 @@
         'security/hr_timesheet_sheet_security.xml',
         'views/hr_timesheet_sheet_views.xml',
         'views/hr_department_views.xml',
+        'views/hr_employee_views.xml',
+        'views/account_analytic_line_views.xml',
         'views/res_config_settings_views.xml',
         'templates/assets.xml',
     ],
