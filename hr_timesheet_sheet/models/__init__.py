aed623d5c713e26761377103c99861c4808313d9
Alexey Pelykh <alexey.pelykh@gmail.com>
2019-07-01 17:46:59 +0300

    [IMP] hr_timesheet_sheet: more hooks

--- hr_timesheet_sheet/models/__init__.py
+++ hr_timesheet_sheet/models/__init__.py
@@ -1,9 +1,9 @@
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from . import account_analytic_account
-from . import account_analytic_line
-from . import hr_department
-from . import hr_employee
-from . import hr_timesheet_sheet
 from . import res_company
 from . import res_config
+from . import account_analytic_account
+from . import account_analytic_line
+from . import hr_timesheet_sheet
+from . import hr_department
+from . import hr_employee
--- hr_timesheet_sheet/models/res_company.py
+++ hr_timesheet_sheet/models/res_company.py
@@ -1,5 +1,6 @@
 # Copyright 2018 Eficent Business and IT Consulting Services, S.L.
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# Copyright 2019 Brainbean Apps (https://brainbeanapps.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import fields, models
 from dateutil.rrule import (MONTHLY, WEEKLY, DAILY)
