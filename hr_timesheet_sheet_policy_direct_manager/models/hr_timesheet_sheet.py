de3d66bce1591e2318178d309bbf0f37a10fdd40
Alexey Pelykh <alexey.pelykh@gmail.com>
2019-11-12 20:31:57 +0000

    [ADD] hr_timesheet_sheet: policies

---
+++ hr_timesheet_sheet_policy_direct_manager/models/hr_timesheet_sheet.py
@@ -0,0 +1,31 @@
+# Copyright 2019 Brainbean Apps (https://brainbeanapps.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from odoo import api, models, _
+from odoo.exceptions import UserError
+
+
+class HrTimesheetSheet(models.Model):
+    _inherit = 'hr_timesheet.sheet'
+
+    @api.multi
+    @api.depends('employee_id.parent_id.user_id')
+    def _compute_direct_manager_as_reviewer(self):
+        self._compute_possible_reviewer_ids()
+
+    @api.multi
+    def _get_possible_reviewers(self):
+        self.ensure_one()
+        if self.review_policy == 'direct_manager':
+            return self.employee_id.parent_id.user_id
+        return super()._get_possible_reviewers()
+
+    @api.multi
+    def _check_can_review(self):
+        super()._check_can_review()
+        if self.filtered(
+                lambda sheet: not sheet.can_review and
+                sheet.review_policy == 'direct_manager'):
+            raise UserError(_(
+                'Only a employee\'s Direct Manager can review the sheet.'
+            ))
