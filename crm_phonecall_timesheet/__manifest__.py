8cb5287bdf7366cdb2d087418b16e9d27bab046b
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2019-12-02 12:17:56 +0100

    [FIX] crm_phonecall_timesheet: Rename module name
    Closes #293

--- crm_phonecall_timesheet/__manifest__.py
+++ crm_phonecall_timesheet/__manifest__.py
@@ -3,7 +3,7 @@
 # Copyright 2017 David Vidal <david.vidal@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    'name': "CRM Timesheet",
+    'name': "CRM Phonecalls Timesheet",
     'category': 'Customer Relationship Management',
     'version': '12.0.1.0.0',
     'depends': [
