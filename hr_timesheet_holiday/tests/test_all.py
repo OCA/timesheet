0607eb8dd9f607c0a9c7eee89109dae30b8e1aad
Holger Brunn <hbrunn@therp.nl>
2020-01-16 09:54:07 +0100

    fixup! [ADD] protect timesheet lines with leaves from deletion

--- hr_timesheet_holiday/tests/test_all.py
+++ hr_timesheet_holiday/tests/test_all.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 import time
 from openerp.tests.common import TransactionCase
-from openerp.exceptions import UserError, ValidationError
+from openerp.exceptions import Warning as UserError, ValidationError
 
 
 class TimesheetHolidayTest(TransactionCase):
