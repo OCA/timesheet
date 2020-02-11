524be882a30760044842208276c6a2d0ca6076e6
Alexis de Lattre <alexis.delattre@akretion.com>
2020-02-11 19:03:50 +0100

    hr_timesheet_sheet: fix FR translation that trigger a crash

--- hr_timesheet_sheet/__manifest__.py
+++ hr_timesheet_sheet/__manifest__.py
@@ -5,7 +5,7 @@
 
 {
     'name': 'HR Timesheet Sheet',
-    'version': '12.0.3.5.0',
+    'version': '12.0.3.5.1',
     'category': 'Human Resources',
     'sequence': 80,
     'summary': 'Timesheet Sheets, Activities',
