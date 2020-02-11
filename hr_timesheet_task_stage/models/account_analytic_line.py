e946e0c7aa660a8c03e6a4e7a996f0bb0fb32ff8
Manuel Calero <manuel.calero@tecnativa.com>
2020-02-11 14:24:14 +0100

    [IMP] hr_timesheet_task_stage: black, isort

--- hr_timesheet_task_stage/models/account_analytic_line.py
+++ hr_timesheet_task_stage/models/account_analytic_line.py
@@ -4,57 +4,61 @@
 # Copyright 2019 Brainbean Apps (https://brainbeanapps.com)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models, api, _
+from odoo import _, api, fields, models
 from odoo.exceptions import UserError
 
 
 class AccountAnalyticLine(models.Model):
-    _inherit = 'account.analytic.line'
+    _inherit = "account.analytic.line"
 
-    is_task_closed = fields.Boolean(
-        related='task_id.stage_id.closed',
-    )
+    is_task_closed = fields.Boolean(related="task_id.stage_id.closed")
 
     @api.multi
     def action_open_task(self):
-        ProjectTaskType = self.env['project.task.type']
+        ProjectTaskType = self.env["project.task.type"]
 
-        for line in self.filtered('task_id.project_id'):
+        for line in self.filtered("task_id.project_id"):
             if line.task_id.project_id:
-                stage = ProjectTaskType.search([
-                    ('project_ids', '=', line.task_id.project_id.id),
-                    ('closed', '=', False)
-                ], limit=1)
+                stage = ProjectTaskType.search(
+                    [
+                        ("project_ids", "=", line.task_id.project_id.id),
+                        ("closed", "=", False),
+                    ],
+                    limit=1,
+                )
                 if not stage:  # pragma: no cover
-                    raise UserError(_(
-                        'There isn\'t any stage with "Closed" unchecked.'
-                        ' Please unmark any.'
-                    ))
-                line.task_id.write({
-                    'stage_id': stage.id,
-                })
+                    raise UserError(
+                        _(
+                            'There isn\'t any stage with "Closed" unchecked.'
+                            " Please unmark any."
+                        )
+                    )
+                line.task_id.write({"stage_id": stage.id})
 
     @api.multi
     def action_close_task(self):
-        ProjectTaskType = self.env['project.task.type']
+        ProjectTaskType = self.env["project.task.type"]
 
-        for line in self.filtered('task_id.project_id'):
-            stage = ProjectTaskType.search([
-                ('project_ids', '=', line.task_id.project_id.id),
-                ('closed', '=', True)
-            ], limit=1)
+        for line in self.filtered("task_id.project_id"):
+            stage = ProjectTaskType.search(
+                [
+                    ("project_ids", "=", line.task_id.project_id.id),
+                    ("closed", "=", True),
+                ],
+                limit=1,
+            )
             if not stage:  # pragma: no cover
-                raise UserError(_(
-                    'There isn\'t any stage with "Closed" checked. Please'
-                    ' mark any.'
-                ))
-            line.task_id.write({
-                'stage_id': stage.id,
-            })
+                raise UserError(
+                    _(
+                        'There isn\'t any stage with "Closed" checked. Please'
+                        " mark any."
+                    )
+                )
+            line.task_id.write({"stage_id": stage.id})
 
     @api.multi
     def action_toggle_task_stage(self):
-        for line in self.filtered('task_id.project_id'):
+        for line in self.filtered("task_id.project_id"):
             if line.is_task_closed:
                 line.action_open_task()
             else:
