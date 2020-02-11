036da5c51a7defee2a119decf9a7a38567164721
Alexey Pelykh <alexey.pelykh@gmail.com>
2020-02-10 17:07:44 -0700

    [ADD] hr_timesheet_sheet_autodraft: automatically create sheets

---
+++ hr_timesheet_sheet_autodraft/models/res_company.py
@@ -0,0 +1,12 @@
+# Copyright 2020 Brainbean Apps (https://brainbeanapps.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class ResCompany(models.Model):
+    _inherit = 'res.company'
+
+    timesheet_sheets_autodraft = fields.Boolean(
+        string='Timesheet Sheets Auto-draft',
+    )
