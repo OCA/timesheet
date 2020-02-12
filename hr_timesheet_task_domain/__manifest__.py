f5c2270377d8577fc74b4ad798e6b3eca656b646
Manuel Calero <manuel.calero@tecnativa.com>
2020-02-12 13:42:14 +0100

    [MIG] hr_timesheet_task_domain: Migration to 13.0

--- hr_timesheet_task_domain/__manifest__.py
+++ hr_timesheet_task_domain/__manifest__.py
@@ -3,11 +3,12 @@
 # Copyright 2016-2018 Tecnativa - Pedro M. Baeza
 # Copyright 2018 Tecnativa - Ernesto Tejeda
 # Copyright 2019 Brainbean Apps (https://brainbeanapps.com)
+# Copyright 2020 Tecnativa - Manuel Calero
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Task Log: limit Task by Project",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "category": "Human Resources",
     "website": "https://github.com/OCA/hr-timesheet",
     "author": "Tecnativa, " "Brainbean Apps, " "Odoo Community Association (OCA)",
