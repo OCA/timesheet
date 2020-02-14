7e2641336c89f384f8f79f0b522f073bb17f259f
Alexey Pelykh <alexey.pelykh@gmail.com>
2020-02-14 15:26:45 -0700

    [FIX] hr_timesheet_sheet: lost kanban mode

--- hr_timesheet_sheet/__manifest__.py
+++ hr_timesheet_sheet/__manifest__.py
@@ -5,7 +5,7 @@
 
 {
     'name': 'HR Timesheet Sheet',
-    'version': '12.0.3.7.0',
+    'version': '12.0.3.7.1',
     'category': 'Human Resources',
     'sequence': 80,
     'summary': 'Timesheet Sheets, Activities',
