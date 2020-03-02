f71d142a4690d1171e54c2cfcfd23881aaeff2a9
flachica <flachica@gitlab.freyi.es>
2020-03-02 10:06:32 +0100

    [IMP] : black, isort

--- hr_timesheet_sheet/__manifest__.py
+++ hr_timesheet_sheet/__manifest__.py
@@ -4,34 +4,28 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'HR Timesheet Sheet',
-    'version': '12.0.3.7.1',
-    'category': 'Human Resources',
-    'sequence': 80,
-    'summary': 'Timesheet Sheets, Activities',
-    'license': 'AGPL-3',
-    'author': (
-        'Eficent, '
-        'Onestein, '
-        'Brainbean Apps, '
-        'Odoo Community Association (OCA)'
+    "name": "HR Timesheet Sheet",
+    "version": "12.0.3.7.1",
+    "category": "Human Resources",
+    "sequence": 80,
+    "summary": "Timesheet Sheets, Activities",
+    "license": "AGPL-3",
+    "author": (
+        "Eficent, " "Onestein, " "Brainbean Apps, " "Odoo Community Association (OCA)"
     ),
-    'website': 'https://github.com/OCA/timesheet',
-    'installable': True,
-    'auto_install': False,
-    'depends': [
-        'hr_timesheet',
-        'web_widget_x2many_2d_matrix',
-    ],
-    'data': [
-        'data/hr_timesheet_sheet_data.xml',
-        'security/ir.model.access.csv',
-        'security/hr_timesheet_sheet_security.xml',
-        'views/hr_timesheet_sheet_views.xml',
-        'views/hr_department_views.xml',
-        'views/hr_employee_views.xml',
-        'views/account_analytic_line_views.xml',
-        'views/res_config_settings_views.xml',
-        'templates/assets.xml',
+    "website": "https://github.com/OCA/timesheet",
+    "installable": True,
+    "auto_install": False,
+    "depends": ["hr_timesheet", "web_widget_x2many_2d_matrix"],
+    "data": [
+        "data/hr_timesheet_sheet_data.xml",
+        "security/ir.model.access.csv",
+        "security/hr_timesheet_sheet_security.xml",
+        "views/hr_timesheet_sheet_views.xml",
+        "views/hr_department_views.xml",
+        "views/hr_employee_views.xml",
+        "views/account_analytic_line_views.xml",
+        "views/res_config_settings_views.xml",
+        "templates/assets.xml",
     ],
 }
--- hr_timesheet_sheet/models/account_analytic_line.py
+++ hr_timesheet_sheet/models/account_analytic_line.py
@@ -2,72 +2,69 @@
 # Copyright 2018-2019 Brainbean Apps (https://brainbeanapps.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models, _
+from odoo import _, api, fields, models
 from odoo.exceptions import UserError, ValidationError
 
 
 class AccountAnalyticLine(models.Model):
-    _inherit = 'account.analytic.line'
+    _inherit = "account.analytic.line"
 
-    sheet_id = fields.Many2one(
-        comodel_name='hr_timesheet.sheet',
-        string='Sheet',
-    )
-    sheet_state = fields.Selection(
-        string='Steet State',
-        related='sheet_id.state',
-    )
+    sheet_id = fields.Many2one(comodel_name="hr_timesheet.sheet", string="Sheet")
+    sheet_state = fields.Selection(string="Steet State", related="sheet_id.state")
 
     @api.multi
     def _get_sheet_domain(self):
         """ Hook for extensions """
         self.ensure_one()
         return [
-            ('date_end', '>=', self.date),
-            ('date_start', '<=', self.date),
-            ('employee_id', '=', self.employee_id.id),
-            ('company_id', 'in', [self.company_id.id, False]),
-            ('state', 'in', ['new', 'draft']),
+            ("date_end", ">=", self.date),
+            ("date_start", "<=", self.date),
+            ("employee_id", "=", self.employee_id.id),
+            ("company_id", "in", [self.company_id.id, False]),
+            ("state", "in", ["new", "draft"]),
         ]
 
     @api.multi
     def _determine_sheet(self):
         """ Hook for extensions """
         self.ensure_one()
-        return self.env['hr_timesheet.sheet'].search(
-            self._get_sheet_domain(),
-            limit=1,
-        )
+        return self.env["hr_timesheet.sheet"].search(self._get_sheet_domain(), limit=1)
 
     def _compute_sheet(self):
         """Links the timesheet line to the corresponding sheet"""
-        for timesheet in self.filtered('project_id'):
+        for timesheet in self.filtered("project_id"):
             sheet = timesheet._determine_sheet()
             if timesheet.sheet_id != sheet:
                 timesheet.sheet_id = sheet
 
     @api.multi
-    @api.constrains('company_id', 'sheet_id')
+    @api.constrains("company_id", "sheet_id")
     def _check_company_id_sheet_id(self):
         for aal in self.sudo():
-            if aal.company_id and aal.sheet_id.company_id and \
-                    aal.company_id != aal.sheet_id.company_id:
-                raise ValidationError(_(
-                    'You cannot create a timesheet of a different company '
-                    'than the one of the timesheet sheet:'
-                    '\n - %s of %s'
-                    '\n - %s of %s' % (
-                        aal.sheet_id.complete_name,
-                        aal.sheet_id.company_id.name,
-                        aal.name,
-                        aal.company_id.name,
+            if (
+                aal.company_id
+                and aal.sheet_id.company_id
+                and aal.company_id != aal.sheet_id.company_id
+            ):
+                raise ValidationError(
+                    _(
+                        "You cannot create a timesheet of a different company "
+                        "than the one of the timesheet sheet:"
+                        "\n - %s of %s"
+                        "\n - %s of %s"
+                        % (
+                            aal.sheet_id.complete_name,
+                            aal.sheet_id.company_id.name,
+                            aal.name,
+                            aal.company_id.name,
+                        )
                     )
-                ))
+                )
 
     @api.model
     def create(self, values):
-        if not self.env.context.get('sheet_create') and 'sheet_id' in values:
-            del values['sheet_id']
+        if not self.env.context.get("sheet_create") and "sheet_id" in values:
+            del values["sheet_id"]
         res = super().create(values)
         res._compute_sheet()
         return res
@@ -98,8 +95,7 @@ class AccountAnalyticLine(models.Model):
     @api.model
     def _timesheet_should_check_write(self, values):
         """ Hook for extensions """
-        return bool(set(self._get_timesheet_protected_fields()) &
-                    set(values.keys()))
+        return bool(set(self._get_timesheet_protected_fields()) & set(values.keys()))
 
     @api.model
     def _timesheet_should_compute_sheet(self, values):
@@ -110,44 +106,41 @@ class AccountAnalyticLine(models.Model):
     def _get_timesheet_protected_fields(self):
         """ Hook for extensions """
         return [
-            'name',
-            'date',
-            'unit_amount',
-            'user_id',
-            'employee_id',
-            'department_id',
-            'company_id',
-            'task_id',
-            'project_id',
-            'sheet_id',
+            "name",
+            "date",
+            "unit_amount",
+            "user_id",
+            "employee_id",
+            "department_id",
+            "company_id",
+            "task_id",
+            "project_id",
+            "sheet_id",
         ]
 
     @api.model
     def _get_sheet_affecting_fields(self):
         """ Hook for extensions """
-        return ['date', 'employee_id', 'project_id', 'company_id']
+        return ["date", "employee_id", "project_id", "company_id"]
 
     @api.multi
     def _check_state(self):
-        if self.env.context.get('skip_check_state'):
+        if self.env.context.get("skip_check_state"):
             return
-        for line in self.filtered('sheet_id'):
-            if line.sheet_id.state not in ['new', 'draft']:
-                raise UserError(_(
-                    'You cannot modify an entry in a confirmed timesheet sheet'
-                    ': %s'
-                ) % (
-                    line.sheet_id.complete_name,
-                ))
+        for line in self.filtered("sheet_id"):
+            if line.sheet_id.state not in ["new", "draft"]:
+                raise UserError(
+                    _(
+                        "You cannot modify an entry in a confirmed timesheet sheet"
+                        ": %s"
+                    )
+                    % (line.sheet_id.complete_name,)
+                )
 
     @api.multi
     def merge_timesheets(self):
-        unit_amount = sum(
-            [t.unit_amount for t in self])
+        unit_amount = sum([t.unit_amount for t in self])
         amount = sum([t.amount for t in self])
-        self[0].write({
-            'unit_amount': unit_amount,
-            'amount': amount,
-        })
+        self[0].write({"unit_amount": unit_amount, "amount": amount})
         self[1:].unlink()
         return self[0]
