496f837e423c151ca308815b6bb03aa80878a238
Carlos Dauden <carlos.dauden@tecnativa.com>
2020-03-13 12:37:48 +0100

    [ADD] sale_timesheet_order_line_sync: New module
    [UPD] Update sale_timesheet_order_line_sync.pot

---
+++ sale_timesheet_order_line_sync/models/project_task.py
@@ -0,0 +1,23 @@
+# Copyright 2020 Tecnativa - Carlos Dauden
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import models
+
+
+class ProjectTask(models.Model):
+    _inherit = 'project.task'
+
+    def write(self, vals):
+        res = super().write(vals)
+        sale_line_id = vals.get('sale_line_id', None)
+        if sale_line_id is None:
+            return res
+        # Avoid rewrite so_line if billable_type is employee_rate
+        self.filtered(
+            lambda t: t.billable_type == 'task_rate'
+        ).mapped('timesheet_ids').filtered(lambda l: (
+            not l.timesheet_invoice_id and l.so_line.id != sale_line_id
+        )).write({
+            'so_line': sale_line_id,
+        })
+        return res
---
+++ sale_timesheet_order_line_sync/readme/USAGE.rst
@@ -0,0 +1,4 @@
+# Go to *Project > All Tasks*
+# Open specific task for editing
+# Set Order Line
+# Open Sale Order to view delivered quantity
