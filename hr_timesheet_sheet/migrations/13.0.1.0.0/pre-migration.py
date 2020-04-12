96dd84add89e0e30d9dd61a10cdeca88a6a504ad
Alexey Pelykh <alexey.pelykh@gmail.com>
2020-04-12 16:45:10 +0200

    [FIX] hr_timesheet_sheet: openupgrade

--- hr_timesheet_sheet/migrations/13.0.1.0.0/pre-migration.py
+++ hr_timesheet_sheet/migrations/13.0.1.0.0/pre-migration.py
@@ -1,6 +1,7 @@
 # Copyright 2020 ForgeFlow
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from openupgradelib import openupgrade
+
+from openupgradelib import openupgrade  # pylint: disable=W7936
 
 column_renames = {
     "res_company": [("sheet_range", None)],
