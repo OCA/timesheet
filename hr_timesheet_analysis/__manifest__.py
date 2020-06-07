f806af91acaef017ff12e2ade3a8739571861999
Alexey Pelykh <alexey.pelykh@gmail.com>
2020-06-07 08:34:30 +0200

    [IMP] hr_timesheet_analysis: specify maintainer and adjust website
    [UPD] README.rst

--- hr_timesheet_analysis/__manifest__.py
+++ hr_timesheet_analysis/__manifest__.py
@@ -1,11 +1,12 @@
-# Copyright 2018 Brainbean Apps
+# Copyright 2018-2020 Brainbean Apps
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Task Logs Analysis',
     'version': '12.0.1.0.0',
     'category': 'Human Resources',
-    'website': 'https://github.com/OCA/hr-timesheet',
+    'maintainers': ['alexey-pelykh'],
+    'website': 'https://github.com/OCA/timesheet',
     'author':
         'Brainbean Apps, '
         'Odoo Community Association (OCA)',
