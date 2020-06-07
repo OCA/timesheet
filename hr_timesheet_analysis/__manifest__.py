c1ee4157fdf637b7ca6ca8bb8757cd9bb70ad839
Alexey Pelykh <alexey.pelykh@gmail.com>
2020-06-07 10:18:13 +0200

    [IMP] hr_timesheet_analysis: black, isort, prettier

--- hr_timesheet_analysis/__manifest__.py
+++ hr_timesheet_analysis/__manifest__.py
@@ -2,22 +2,16 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Task Logs Analysis',
-    'version': '12.0.1.0.0',
-    'category': 'Human Resources',
-    'maintainers': ['alexey-pelykh'],
-    'website': 'https://github.com/OCA/timesheet',
-    'author':
-        'Brainbean Apps, '
-        'Odoo Community Association (OCA)',
-    'license': 'AGPL-3',
-    'installable': True,
-    'application': False,
-    'summary': 'Analyze tracked time in Pivot, Graph views',
-    'depends': [
-        'hr_timesheet',
-    ],
-    'data': [
-        'views/account_analytic_line.xml',
-    ],
+    "name": "Task Logs Analysis",
+    "version": "12.0.1.0.0",
+    "category": "Human Resources",
+    "maintainers": ["alexey-pelykh"],
+    "website": "https://github.com/OCA/timesheet",
+    "author": "Brainbean Apps, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "installable": True,
+    "application": False,
+    "summary": "Analyze tracked time in Pivot, Graph views",
+    "depends": ["hr_timesheet"],
+    "data": ["views/account_analytic_line.xml"],
 }
