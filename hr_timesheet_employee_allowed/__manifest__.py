d57b912f62312a3838d1bf0f8542bf7fceb36e92
Chandresh Thakkar <chandresh.thakkar.serpentcs@gmail.com>
2020-06-15 22:57:10 +0530

    [NEW] Added a module to restrict the user to add any employee in timesheet lines.

---
+++ hr_timesheet_employee_allowed/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright (C) 2020 Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Timesheet Employees Allowed",
+    "summary": "Limits the Employees that are allowed to enter \
+    timesheets for others employees.",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Open Source Integrators, Odoo Community Association (OCA)",
+    "category": "Timesheet",
+    "website": "https://github.com/OCA/timesheet",
+    "depends": ["hr_timesheet"],
+    "data": [
+        "views/hr_employee_view.xml",
+        "views/account_analytic_line_view.xml",
+    ],
+    "installable": True,
+}
