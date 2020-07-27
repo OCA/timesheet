66842e0974e349c7557af896a5a742721650546b
David James <david@djdc.net.au>
2020-07-27 22:16:41 +1000

    [IMP] hr_timesheet_activity_begin_end: black, isort, prettier

--- hr_timesheet_activity_begin_end/__manifest__.py
+++ hr_timesheet_activity_begin_end/__manifest__.py
@@ -2,19 +2,15 @@
 # Copyright 2017 Tecnativa, S.L. - Luis M. Ontalba
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-{'name': 'Timesheet Activities - Begin/End Hours',
- 'version': '12.0.1.1.0',
- 'author':  'Camptocamp, '
-            'Tecnativa, '
-            'Odoo Community Association (OCA)',
- 'license': 'AGPL-3',
- 'category': 'Human Resources',
- 'depends': ['hr_timesheet_sheet',
-             ],
- 'website': 'https://github.com/OCA/timesheet',
- 'data': ['views/hr_analytic_timesheet.xml',
-          'views/hr_timesheet_sheet.xml',
-          ],
- 'installable': True,
- 'auto_install': False,
- }
+{
+    "name": "Timesheet Activities - Begin/End Hours",
+    "version": "12.0.1.1.0",
+    "author": "Camptocamp, Tecnativa, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "category": "Human Resources",
+    "depends": ["hr_timesheet_sheet"],
+    "website": "https://github.com/OCA/timesheet",
+    "data": ["views/hr_analytic_timesheet.xml", "views/hr_timesheet_sheet.xml"],
+    "installable": True,
+    "auto_install": False,
+}
