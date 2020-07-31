d88a80282aea269d678d8665b558d27f334f2a9b
David James <david@djdc.net.au>
2020-07-31 17:24:06 +1000

    [IMP] hr_timesheet_sheet_autodraft: black, isort, prettier
    [IMP] hr_timesheet_sheet_autodraft: black, isort, prettier

--- hr_timesheet_sheet_autodraft/models/account_analytic_line.py
+++ hr_timesheet_sheet_autodraft/models/account_analytic_line.py
@@ -5,7 +5,7 @@ from odoo import api, models
 
 
 class AccountAnalyticLine(models.Model):
-    _inherit = 'account.analytic.line'
+    _inherit = "account.analytic.line"
 
     @api.multi
     def _determine_sheet(self):
@@ -18,19 +18,17 @@ class AccountAnalyticLine(models.Model):
     def _autodraft_sheet(self):
         """ Hook for extensions """
         self.ensure_one()
-        HrTimesheetSheet = self.env['hr_timesheet.sheet']
-        if not self.env.context.get('manual_autodraft_timesheet_sheet', False):
+        HrTimesheetSheet = self.env["hr_timesheet.sheet"]
+        if not self.env.context.get("manual_autodraft_timesheet_sheet", False):
             HrTimesheetSheet = HrTimesheetSheet.sudo()
         values = self._get_autodraft_sheet_values()
-        sheet = HrTimesheetSheet.new({
-            **HrTimesheetSheet.default_get(HrTimesheetSheet._fields.keys()),
-            **values,
-        })
+        sheet = HrTimesheetSheet.new(
+            {**HrTimesheetSheet.default_get(HrTimesheetSheet._fields.keys()), **values}
+        )
         existing_sheets_domain = sheet._get_overlapping_sheet_domain()
-        existing_sheets_domain = list(filter(
-            lambda criteria: criteria[0] != 'id',
-            existing_sheets_domain
-        ))
+        existing_sheets_domain = list(
+            filter(lambda criteria: criteria[0] != "id", existing_sheets_domain)
+        )
         if HrTimesheetSheet.search(existing_sheets_domain, limit=1):
             return None
         sheet = HrTimesheetSheet.create(values)
@@ -41,22 +39,18 @@ class AccountAnalyticLine(models.Model):
     def _get_autodraft_sheet_values(self):
         """ Hook for extensions """
         self.ensure_one()
-        HrTimesheetSheet = self.env['hr_timesheet.sheet']
+        HrTimesheetSheet = self.env["hr_timesheet.sheet"]
         return {
-            'employee_id': self.employee_id.id,
-            'company_id': self.company_id.id,
-            'date_start': HrTimesheetSheet._get_period_start(
-                self.company_id,
-                self.date,
+            "employee_id": self.employee_id.id,
+            "company_id": self.company_id.id,
+            "date_start": HrTimesheetSheet._get_period_start(
+                self.company_id, self.date
             ),
-            'date_end': HrTimesheetSheet._get_period_end(
-                self.company_id,
-                self.date,
-            ),
+            "date_end": HrTimesheetSheet._get_period_end(self.company_id, self.date),
         }
 
     @api.multi
     def action_autodraft_timesheet_sheets(self):
         self.filtered(lambda aal: not aal.sheet_id).with_context(
-            manual_autodraft_timesheet_sheet=True,
+            manual_autodraft_timesheet_sheet=True
         )._compute_sheet()
