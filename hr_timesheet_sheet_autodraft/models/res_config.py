269df3d2e8d1727da51ce56bb1b7aad64f81abce
David James <david@djdc.net.au>
2020-07-31 17:24:06 +1000

    [IMP] hr_timesheet_sheet_autodraft: black, isort, prettier
    [IMP] hr_timesheet_sheet_autodraft: black, isort, prettier

--- hr_timesheet_sheet_autodraft/models/res_config.py
+++ hr_timesheet_sheet_autodraft/models/res_config.py
@@ -5,15 +5,15 @@ from odoo import fields, models
 
 
 class ResConfig(models.TransientModel):
-    _inherit = 'res.config.settings'
+    _inherit = "res.config.settings"
 
     timesheet_sheets_autodraft = fields.Boolean(
-        related='company_id.timesheet_sheets_autodraft',
-        string='Timesheet Sheets Auto-draft',
+        related="company_id.timesheet_sheets_autodraft",
+        string="Timesheet Sheets Auto-draft",
         help=(
-            'Auto-draft Timesheet Sheets whenever a Timesheet entry is created'
-            ' or modified to ensure it\'s covered by a relevant Timesheet'
-            ' Sheet'
+            "Auto-draft Timesheet Sheets whenever a Timesheet entry is created"
+            " or modified to ensure it's covered by a relevant Timesheet"
+            " Sheet"
         ),
         readonly=False,
     )
