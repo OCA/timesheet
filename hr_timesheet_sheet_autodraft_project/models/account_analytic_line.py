8aba40f0567e63e0e2fdd093ca55552174b0a1ee
David James <david@djdc.net.au>
2020-08-01 11:57:52 +1000

    [IMP] hr_timesheet_sheet_autodraft_project: black, isort, prettier

--- hr_timesheet_sheet_autodraft_project/models/account_analytic_line.py
+++ hr_timesheet_sheet_autodraft_project/models/account_analytic_line.py
@@ -5,13 +5,11 @@ from odoo import api, models
 
 
 class AccountAnalyticLine(models.Model):
-    _inherit = 'account.analytic.line'
+    _inherit = "account.analytic.line"
 
     @api.multi
     def _get_autodraft_sheet_values(self):
         values = super()._get_autodraft_sheet_values()
-        if self.company_id.timesheet_sheet_review_policy == 'project_manager':
-            values.update({
-                'project_id': self.project_id.id,
-            })
+        if self.company_id.timesheet_sheet_review_policy == "project_manager":
+            values.update({"project_id": self.project_id.id})
         return values
