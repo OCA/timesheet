c1a6269c5e7727e6517c7fbe0a8db86fe4f9cc4b
flachica <fernandolachica@gmail.com>
2020-08-24 13:48:24 +0200

    [IMP] sale_timesheet_task_exclude: black, isort

--- sale_timesheet_task_exclude/__manifest__.py
+++ sale_timesheet_task_exclude/__manifest__.py
@@ -2,21 +2,15 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Sales Timesheet: exclude Task from Sale Order',
-    'version': '12.0.1.0.0',
-    'category': 'Sales',
-    'website': 'https://github.com/OCA/timesheet',
-    'author':
-        'Brainbean Apps, '
-        'Odoo Community Association (OCA)',
-    'license': 'AGPL-3',
-    'installable': True,
-    'application': False,
-    'summary': 'Exclude Task and related Timesheets from Sale Order',
-    'depends': [
-        'sale_timesheet',
-    ],
-    'data': [
-        'views/project_task.xml',
-    ],
+    "name": "Sales Timesheet: exclude Task from Sale Order",
+    "version": "12.0.1.0.0",
+    "category": "Sales",
+    "website": "https://github.com/OCA/timesheet",
+    "author": "Brainbean Apps, " "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "installable": True,
+    "application": False,
+    "summary": "Exclude Task and related Timesheets from Sale Order",
+    "depends": ["sale_timesheet",],
+    "data": ["views/project_task.xml",],
 }
