e9e14921c5f1fda4d50f37e86f785e7f3e254676
flachica <fernandolachica@gmail.com>
2020-08-24 13:50:01 +0200

    [13.0] sale_timesheet_task_exclude: Migration to 13.0

--- sale_timesheet_task_exclude/__manifest__.py
+++ sale_timesheet_task_exclude/__manifest__.py
@@ -3,14 +3,14 @@
 
 {
     "name": "Sales Timesheet: exclude Task from Sale Order",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "category": "Sales",
     "website": "https://github.com/OCA/timesheet",
-    "author": "Brainbean Apps, " "Odoo Community Association (OCA)",
+    "author": "Brainbean Apps, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "installable": True,
     "application": False,
     "summary": "Exclude Task and related Timesheets from Sale Order",
-    "depends": ["sale_timesheet",],
-    "data": ["views/project_task.xml",],
+    "depends": ["sale_timesheet"],
+    "data": ["views/project_task.xml"],
 }
