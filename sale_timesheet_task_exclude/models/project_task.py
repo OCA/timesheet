a62e5d2385fa652aaa81607b695e95a6e1e2b5c7
flachica <fernandolachica@gmail.com>
2020-08-24 13:48:24 +0200

    [IMP] sale_timesheet_task_exclude: black, isort

--- sale_timesheet_task_exclude/models/project_task.py
+++ sale_timesheet_task_exclude/models/project_task.py
@@ -1,31 +1,31 @@
 # Copyright 2019 Brainbean Apps (https://brainbeanapps.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models, api
+from odoo import api, fields, models
 
 
 class ProjectTask(models.Model):
-    _inherit = 'project.task'
+    _inherit = "project.task"
 
     exclude_from_sale_order = fields.Boolean(
-        string='Exclude from Sale Order',
+        string="Exclude from Sale Order",
         help=(
-            'Checking this would exclude any timesheet entries logged towards'
-            ' this task from Sale Order'
+            "Checking this would exclude any timesheet entries logged towards"
+            " this task from Sale Order"
         ),
     )
 
     @api.multi
-    @api.depends('exclude_from_sale_order')
+    @api.depends("exclude_from_sale_order")
     def _compute_billable_type(self):
         super()._compute_billable_type()
-        for task in self.filtered('exclude_from_sale_order'):
-            task.billable_type = 'no'
+        for task in self.filtered("exclude_from_sale_order"):
+            task.billable_type = "no"
 
     @api.multi
     def write(self, vals):
         res = super().write(vals)
-        if 'exclude_from_sale_order' in vals:
+        if "exclude_from_sale_order" in vals:
             # If tasks changed their exclude_from_sale_order, update all AALs
             # that have not been invoiced yet
             for timesheet in self.timesheet_ids.filtered(
