d1263ede1691f3f8b223e5b1aa8405097b4c6c12
sergiocorato <sergiocorato@gmail.com>
2020-09-22 17:29:41 +0200

    [FIX] to force pip

--- sale_timesheet_existing_project/models/sale_order.py
+++ sale_timesheet_existing_project/models/sale_order.py
@@ -1,4 +1,4 @@
-# Copyright 2019 Tecnativa - Pedro M. Baeza
+# Copyright 2019 Tecnativa -  Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import api, fields, models
