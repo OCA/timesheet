9f21c2f2ca52f08e439041741fb643090ff58512
João Marques <joao.marques@tecnativa.com>
2020-09-23 16:02:04 +0200

    [IMP] sale_timesheet_order_line_sync: black, isort, prettier

--- sale_timesheet_order_line_sync/__manifest__.py
+++ sale_timesheet_order_line_sync/__manifest__.py
@@ -2,18 +2,14 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Sale Timesheet Order Line Sync',
-    'version': '12.0.1.0.1',
-    'category': 'Sales',
-    'website': 'https://github.com/OCA/timesheet',
-    'author':
-        'Tecnativa, '
-        'Odoo Community Association (OCA)',
-    'license': 'AGPL-3',
-    'installable': True,
-    'application': False,
-    'summary': 'Propagate task order line in not invoiced timesheet lines',
-    'depends': [
-        'sale_timesheet',
-    ],
+    "name": "Sale Timesheet Order Line Sync",
+    "version": "12.0.1.0.1",
+    "category": "Sales",
+    "website": "https://github.com/OCA/timesheet",
+    "author": "Tecnativa, " "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "installable": True,
+    "application": False,
+    "summary": "Propagate task order line in not invoiced timesheet lines",
+    "depends": ["sale_timesheet",],
 }
