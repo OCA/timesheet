39742927dc059858d9dd4100c2307032372d6ca3
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2020-09-26 13:23:36 +0200

    [IMP] crm_timesheet: black, isort, prettier

--- crm_timesheet/models/account_analytic_line.py
+++ crm_timesheet/models/account_analytic_line.py
@@ -7,14 +7,11 @@ from odoo import api, fields, models
 
 
 class AccountAnalyticLine(models.Model):
-    _inherit = 'account.analytic.line'
+    _inherit = "account.analytic.line"
 
-    lead_id = fields.Many2one(
-        comodel_name='crm.lead',
-        string='Lead/Opportunity',
-    )
+    lead_id = fields.Many2one(comodel_name="crm.lead", string="Lead/Opportunity",)
 
-    @api.onchange('lead_id')
+    @api.onchange("lead_id")
     def _onchange_lead_id(self):
         if self.lead_id.project_id:
             self.project_id = self.lead_id.project_id.id
