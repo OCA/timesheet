898ed61c6280173a7567fd719c236254fd5058fd
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2020-09-26 13:23:36 +0200

    [IMP] crm_timesheet: black, isort, prettier

--- crm_timesheet/tests/test_account_analytic_line.py
+++ crm_timesheet/tests/test_account_analytic_line.py
@@ -2,6 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from datetime import date, datetime, timedelta
+
 from odoo import exceptions
 from odoo.tests.common import SavepointCase
 
@@ -11,24 +12,19 @@ class AccountAnalyticLineCase(SavepointCase):
     def setUpClass(cls):
         super().setUpClass()
         admin = cls.env.ref("base.user_admin")
-        admin.groups_id |= (
-            cls.env.ref("hr_timesheet.group_hr_timesheet_user") |
-            cls.env.ref("sales_team.group_sale_salesman")
-        )
+        admin.groups_id |= cls.env.ref(
+            "hr_timesheet.group_hr_timesheet_user"
+        ) | cls.env.ref("sales_team.group_sale_salesman")
         env = cls.env(user=admin)
         Account = env["account.analytic.account"]
         Project = env["project.project"]
-        cls.account1 = Account.create({
-            "name": "Test Account 1",
-        })
-        cls.project1 = Project.create({
-            "name": "Test Project 1",
-            "analytic_account_id": cls.account1.id,
-        })
-        cls.lead = env['crm.lead'].create({
-            'name': 'Test lead',
-            'project_id': cls.project1.id,
-        })
+        cls.account1 = Account.create({"name": "Test Account 1"})
+        cls.project1 = Project.create(
+            {"name": "Test Project 1", "analytic_account_id": cls.account1.id}
+        )
+        cls.lead = env["crm.lead"].create(
+            {"name": "Test lead", "project_id": cls.project1.id}
+        )
 
     def setUp(self):
         super().setUp()
@@ -45,31 +41,35 @@ class AccountAnalyticLineCase(SavepointCase):
         self.assertEqual(action["type"], "ir.actions.act_window")
         self.assertEqual(action["view_mode"], "form")
         self.assertEqual(action["view_type"], "form")
-        return active_record.env[action["res_model"]].with_context(
-            active_id=active_record.id,
-            active_ids=active_record.ids,
-            active_model=active_record._name,
-            **action.get("context", {}),
-        ).create({})
+        return (
+            active_record.env[action["res_model"]]
+            .with_context(
+                active_id=active_record.id,
+                active_ids=active_record.ids,
+                active_model=active_record._name,
+                **action.get("context", {}),
+            )
+            .create({})
+        )
 
     def test_onchange_lead(self):
         """Changing the lead changes the associated project."""
-        line = self.env["account.analytic.line"].new({
-            "lead_id": self.lead.id,
-        })
+        line = self.env["account.analytic.line"].new({"lead_id": self.lead.id})
         line._onchange_lead_id()
         self.assertEqual(line.project_id, self.project1)
 
     def test_lead_time_control_flow(self):
         """Test crm.lead time controls."""
         # A timer is running
-        previous_line = self.env["account.analytic.line"].create({
-            'date_time': datetime.now() - timedelta(hours=1),
-            'lead_id': self.lead.id,
-            'project_id': self.project1.id,
-            'account_id': self.account1.id,
-            'name': 'Test line',
-        })
+        previous_line = self.env["account.analytic.line"].create(
+            {
+                "date_time": datetime.now() - timedelta(hours=1),
+                "lead_id": self.lead.id,
+                "project_id": self.project1.id,
+                "account_id": self.account1.id,
+                "name": "Test line",
+            }
+        )
         self.assertFalse(previous_line.unit_amount)
         # Running line found, stop the timer
         self.assertEqual(self.lead.show_time_control, "stop")
@@ -86,9 +86,7 @@ class AccountAnalyticLineCase(SavepointCase):
         self.assertLessEqual(wizard.date_time, datetime.now())
         self.assertLessEqual(wizard.date, date.today())
         self.assertFalse(wizard.unit_amount)
-        self.assertEqual(
-            wizard.account_id,
-            self.lead.project_id.analytic_account_id)
+        self.assertEqual(wizard.account_id, self.lead.project_id.analytic_account_id)
         self.assertEqual(wizard.employee_id, self.env.user.employee_ids)
         self.assertEqual(wizard.name, previous_line.name)
         self.assertEqual(wizard.project_id, self.lead.project_id)
