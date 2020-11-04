a5ac3d6eba9a8e96e70b8f9adb9c20943763fe69
Alexey Pelykh <alexey.pelykh@gmail.com>
2020-11-04 12:44:34 +0200

    [UPD] Brainbean Apps => CorporateHub

--- hr_timesheet_sheet_policy_direct_manager/__manifest__.py
+++ hr_timesheet_sheet_policy_direct_manager/__manifest__.py
@@ -1,11 +1,12 @@
 # Copyright 2019 Brainbean Apps (https://brainbeanapps.com)
+# Copyright 2020 CorporateHub (https://corporatehub.eu)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
     'name': 'HR Timesheet Sheet: Direct Manager Policy',
     'version': '12.0.1.2.0',
     'author':
-        'Brainbean Apps, '
+        'CorporateHub, '
         'Odoo Community Association (OCA)',
     'website': 'https://github.com/OCA/timesheet/',
     'license': 'AGPL-3',
