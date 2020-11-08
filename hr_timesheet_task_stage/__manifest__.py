f9f5b09b22d1b4870974cd1f0c781590cbee2b2d
Alexey Pelykh <alexey.pelykh@gmail.com>
2020-11-08 08:11:32 +0200

    [UPD] Brainbean Apps => CorporateHub

--- hr_timesheet_task_stage/__manifest__.py
+++ hr_timesheet_task_stage/__manifest__.py
@@ -3,6 +3,7 @@
 # Copyright 2016-2018 Tecnativa - Pedro M. Baeza
 # Copyright 2018 Tecnativa - Ernesto Tejeda
 # Copyright 2019 Brainbean Apps (https://brainbeanapps.com)
+# Copyright 2020 CorporateHub (https://corporatehub.eu)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
@@ -10,7 +11,7 @@
     "version": "13.0.1.1.0",
     "category": "Operations/Timesheets",
     "website": "https://github.com/OCA/timesheet",
-    "author": "Tecnativa, " "Brainbean Apps, " "Odoo Community Association (OCA)",
+    "author": "Tecnativa, " "CorporateHub, " "Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "installable": True,
     "application": False,
