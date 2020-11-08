a4ca8e93616a7dd084818486194ff48abd618c36
Alexey Pelykh <alexey.pelykh@gmail.com>
2020-11-08 08:11:32 +0200

    [UPD] Brainbean Apps => CorporateHub

--- hr_timesheet_task_required/__manifest__.py
+++ hr_timesheet_task_required/__manifest__.py
@@ -1,5 +1,6 @@
 # Copyright 2016-2017 ACSONE SA/NV
 # Copyright 2018-2019 Brainbean Apps (https://brainbeanapps.com)
+# Copyright 2020 CorporateHub (https://corporatehub.eu)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
@@ -8,7 +9,7 @@
         Set task on timesheet as a mandatory field""",
     "version": "13.0.1.0.0",
     "license": "AGPL-3",
-    "author": "ACSONE SA/NV, Brainbean Apps, Odoo Community Association (OCA)",
+    "author": "ACSONE SA/NV, CorporateHub, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/timesheet",
     "depends": ["hr_timesheet"],
     "data": [
