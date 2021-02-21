0712b78dc9bd8cff63b44ad2f149d3f4745d374f
Holger Brunn <mail@hunki-enterprises.com>
2021-02-21 07:10:53 +0100

    [ADD] hr_timesheet_portal

---
+++ hr_timesheet_portal/__manifest__.py
@@ -0,0 +1,26 @@
+# Copyright 2021 Hunki Enterprises BV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Timesheet portal (editable)",
+    "summary": "Fill in timesheets via the portal",
+    "version": "12.0.1.0.0",
+    "development_status": "Alpha",
+    "category": "Website",
+    "website": "https://github.com/OCA/hr-timesheet",
+    "author": "Hunki Enterprises BV, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "depends": [
+        "hr_timesheet",
+        "portal",
+        "website",
+    ],
+    "data": [
+        "templates/assets.xml",
+        "templates/portal.xml",
+        "security/hr_timesheet_portal_security.xml",
+        "security/ir.model.access.csv",
+    ],
+    "demo": [
+        "demo/hr_timesheet_portal.xml",
+    ],
+}
