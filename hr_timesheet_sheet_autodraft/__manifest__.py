ef7c8973b194a0bb66d19d6c7f457e207768085e
dsolanki <dhara.solanki@initos.com>
2021-03-09 12:36:02 +0530

    [IMP] hr_timesheet_sheet_autodraft: isort, black, prettier.

--- hr_timesheet_sheet_autodraft/__manifest__.py
+++ hr_timesheet_sheet_autodraft/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "HR Timesheet Sheet Auto-draft",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Human Resources",
     "website": "https://github.com/OCA/timesheet",
     "author": "CorporateHub, Odoo Community Association (OCA)",
