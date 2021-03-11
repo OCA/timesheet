a4639ea49ae54ca23b72cd2adb318569859eb111
fshah-initos <foram.shah@initos.com>
2021-03-11 16:59:59 +0530

    [IMP] sale_timesheet_rounded: black, isort, prettier

--- sale_timesheet_rounded/__manifest__.py
+++ sale_timesheet_rounded/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Sale Timesheet Rounded",
     "summary": "Round timesheet entries amount based on project settings.",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "author": "Camptocamp, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "category": "Sales",
