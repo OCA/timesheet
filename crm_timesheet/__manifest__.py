7251cc3aa02df8d32586cba07ac1c16c29c87dbc
hkapatel <helly.kapatel@initos.com>
2021-03-15 14:46:38 +0530

    [MIG] Migrate module crm_timesheet to v14.

--- crm_timesheet/__manifest__.py
+++ crm_timesheet/__manifest__.py
@@ -10,7 +10,11 @@
     "category": "Customer Relationship Management",
     "version": "14.0.1.0.0",
     "depends": ["crm", "project_timesheet_time_control"],
-    "data": ["views/crm_lead_view.xml", "views/hr_timesheet_view.xml"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/crm_lead_view.xml",
+        "views/hr_timesheet_view.xml",
+    ],
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/timesheet",
     "license": "AGPL-3",
