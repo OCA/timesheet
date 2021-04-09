5fe78ed7edcc432b065e7fa0ef8fe745b9d00c39
AmmarOfficewalaSerpentCS <ammar.o.serpentcs@gmail.com>
2021-04-09 18:07:27 +0530

    [FIX] Fixed travis.

--- hr_timesheet_time_type/models/account_analytic_line.py
+++ hr_timesheet_time_type/models/account_analytic_line.py
@@ -8,6 +8,10 @@ class AccountAnalyticLine(models.Model):
     _inherit = "account.analytic.line"
 
     time_type_id = fields.Many2one(
-        comodel_name="project.time.type", string="Time Type",
+        comodel_name="project.time.type",
+        string="Time Type",
     )
-    time_type_name = fields.Char(related="time_type_id.name", string="Time Type Name",)
+    time_type_name = fields.Char(
+        related="time_type_id.name",
+        string="Time Type Name",
+    )
