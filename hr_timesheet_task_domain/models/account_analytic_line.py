396f50d5e826eb4a6e6faf453967d36c862d58d6
fshah <foram.shah@initos.com>
2021-05-04 16:54:43 +0530

    [MIG] Migrate module hr_timesheet_task_domain to v14.

--- hr_timesheet_task_domain/models/account_analytic_line.py
+++ hr_timesheet_task_domain/models/account_analytic_line.py
@@ -11,21 +11,16 @@ class AccountAnalyticLine(models.Model):
     _inherit = "account.analytic.line"
 
     @api.onchange("project_id")
-    def onchange_project_id(self):
-        # Check if 'closed' field exists (provided by project_stage_closed)
-        project_stage_closed = (
-            "project.task.type" in self.env
-            and "closed" in self.env["project.task.type"]._fields
-        )
-
+    def _onchange_project_id(self):
         task = self.task_id
-        res = super().onchange_project_id()
+        res = super()._onchange_project_id()
         if res is None:
             res = {}
         if self.project_id:  # Show only opened tasks
-            task_domain = [("project_id", "=", self.project_id.id)]
-            if project_stage_closed:
-                task_domain = task_domain + [("stage_id.closed", "=", False)]
+            task_domain = [
+                ("project_id", "=", self.project_id.id),
+                ("stage_id.is_closed", "=", False),
+            ]
             res_domain = res.setdefault("domain", {})
             res_domain.update({"task_id": task_domain})
         else:  # Reset domain for allowing selection of any task
@@ -34,10 +29,3 @@ class AccountAnalyticLine(models.Model):
             # Restore previous task if belongs to the same project
             self.task_id = task
         return res
-
-    @api.onchange("task_id")
-    def _onchange_task_id(self):
-        super()._onchange_task_id()
-
-        if self.task_id:
-            self.project_id = self.task_id.project_id
