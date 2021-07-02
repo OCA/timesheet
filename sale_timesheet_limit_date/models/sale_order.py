5462be65638ae718481edd39beb75a3931fd3907
MrTango <md@derico.de>
2021-07-02 18:33:08 +0300

    [IMP] sale_timesheet_limit_date: black, isort, prettier

--- sale_timesheet_limit_date/models/sale_order.py
+++ sale_timesheet_limit_date/models/sale_order.py
@@ -5,31 +5,28 @@ from odoo import api, fields, models
 
 
 class SaleOrder(models.Model):
-    _inherit = 'sale.order'
+    _inherit = "sale.order"
 
     timesheet_limit_date = fields.Date(
-        string='Timesheet Limit Date',
-        help='Invoice will be created with timesheet prior and including'
-        ' the date set',
+        string="Timesheet Limit Date",
+        help="Invoice will be created with timesheet prior and including"
+        " the date set",
     )
 
     @api.multi
-    @api.depends('timesheet_limit_date')
+    @api.depends("timesheet_limit_date")
     def _compute_timesheet_ids(self):
         # this method copy of base method, it injects date in domain
         for order in self:
             if order.analytic_account_id:
                 domain = [
-                    ('so_line', 'in', order.order_line.ids),
-                    ('amount', '<=', 0.0),
-                    ('project_id', '!=', False),
+                    ("so_line", "in", order.order_line.ids),
+                    ("amount", "<=", 0.0),
+                    ("project_id", "!=", False),
                 ]
                 if order.timesheet_limit_date:
-                    domain.append(
-                        ('date', '<=', order.timesheet_limit_date)
-                    )
-                order.timesheet_ids = self.env[
-                    'account.analytic.line'].search(domain)
+                    domain.append(("date", "<=", order.timesheet_limit_date))
+                order.timesheet_ids = self.env["account.analytic.line"].search(domain)
             else:
                 order.timesheet_ids = []
             order.timesheet_count = len(order.timesheet_ids)
@@ -39,7 +36,7 @@ class SaleOrder(models.Model):
         # and drop limit date from sale order
         res = super().action_invoice_create(grouped, final)
         orders_with_ts_limit = self.filtered(lambda o: o.timesheet_limit_date)
-        orders_with_ts_limit.write({'timesheet_limit_date': False})
+        orders_with_ts_limit.write({"timesheet_limit_date": False})
         return res
 
     @api.multi
@@ -47,5 +44,5 @@ class SaleOrder(models.Model):
         # set appropriate limit date to new created invoice
         res = super()._prepare_invoice()
         if self.timesheet_limit_date:
-            res['timesheet_limit_date'] = self.timesheet_limit_date
+            res["timesheet_limit_date"] = self.timesheet_limit_date
         return res
