544baec13692c2985588fd43b2d91bc70c05e257
MrTango <md@derico.de>
2021-07-05 17:19:44 +0300

    replace 12.0 with 13.0 in README, manifest & co

--- sale_timesheet_limit_date/__manifest__.py
+++ sale_timesheet_limit_date/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Invoice Sales Timesheets with a Date Limit",
     "summary": "Layouts",
-    "version": "12.0.1.0.1",
+    "version": "13.0.1.0.0",
     "author": "Camptocamp, " "Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "category": "Sales",
