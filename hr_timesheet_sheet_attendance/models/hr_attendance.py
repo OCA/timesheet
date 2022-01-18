d61dd78a6146a739d4865daaa9028b4e43798665
Ernesto Tejeda <ernesto.tejeda@tecnativa.com>
2022-01-18 15:36:34 -0500

    [IMP] hr_timesheet_sheet_attendance: black, isort, prettier

--- hr_timesheet_sheet_attendance/models/hr_attendance.py
+++ hr_timesheet_sheet_attendance/models/hr_attendance.py
@@ -1,11 +1,11 @@
-from odoo import api, fields, models, _
-from odoo.exceptions import UserError
-
 import pytz
+
+from odoo import _, api, fields, models
+from odoo.exceptions import UserError
 
 
 class HrAttendance(models.Model):
-    _inherit = 'hr.attendance'
+    _inherit = "hr.attendance"
 
     def _get_attendance_employee_tz(self, date=None):
         """Convert date according to timezone of user
@@ -17,7 +17,7 @@ class HrAttendance(models.Model):
             tz = self.employee_id.user_id.partner_id.tz
         if not date:
             return False
-        time_zone = pytz.timezone(tz or 'UTC')
+        time_zone = pytz.timezone(tz or "UTC")
         attendance_tz_dt = pytz.UTC.localize(date)
         attendance_tz_dt = attendance_tz_dt.astimezone(time_zone)
         return attendance_tz_dt.date()
@@ -26,22 +26,19 @@ class HrAttendance(models.Model):
         """Find and return current timesheet-sheet
         :return: recordset of hr_timesheet.sheet or False"""
 
-        sheet_obj = self.env['hr_timesheet.sheet']
+        sheet_obj = self.env["hr_timesheet.sheet"]
         check_in = False
         if self.check_in:
             check_in = self._get_attendance_employee_tz(date=self.check_in)
 
-        domain = [('employee_id', '=', self.employee_id.id)]
+        domain = [("employee_id", "=", self.employee_id.id)]
         if check_in:
-            domain += [
-                ('date_start', '<=', check_in),
-                ('date_end', '>=', check_in)
-            ]
+            domain += [("date_start", "<=", check_in), ("date_end", ">=", check_in)]
 
         sheet_ids = sheet_obj.search(domain, limit=1)
         return sheet_ids[:1] or False
 
-    @api.depends('employee_id', 'check_in', 'check_out')
+    @api.depends("employee_id", "check_in", "check_out")
     def _compute_sheet_id(self):
         """Find and set current timesheet-sheet in
         current attendance record"""
@@ -49,19 +46,18 @@ class HrAttendance(models.Model):
             attendance.sheet_id = attendance._get_timesheet_sheet()
 
     sheet_id = fields.Many2one(
-        comodel_name='hr_timesheet.sheet',
+        comodel_name="hr_timesheet.sheet",
         compute="_compute_sheet_id",
-        string='Sheet',
-        store=True)
+        string="Sheet",
+        store=True,
+    )
 
     def _check_timesheet_state(self):
         """Check and raise error if current sheet not in draftstate"""
-        if self._context.get('allow_modify_confirmed_sheet', False):
+        if self._context.get("allow_modify_confirmed_sheet", False):
             return
-        if self.sheet_id and self.sheet_id.state != 'draft':
-            raise UserError(_(
-                "You cannot modify an entry in a confirmed timesheet"
-            ))
+        if self.sheet_id and self.sheet_id.state != "draft":
+            raise UserError(_("You cannot modify an entry in a confirmed timesheet"))
 
     @api.multi
     def unlink(self):
@@ -71,7 +67,7 @@ class HrAttendance(models.Model):
 
         return super(HrAttendance, self).unlink()
 
-    @api.constrains('check_in', 'check_out')
+    @api.constrains("check_in", "check_out")
     def _check_timesheet(self):
         """- Restrict to create attendance in confirmed timesheet-sheet
         - Restrict to add attendance date outside the current
@@ -79,24 +75,30 @@ class HrAttendance(models.Model):
         timesheet = self.sheet_id
         if not timesheet:
             return
-        if timesheet and timesheet.state != 'draft':
-            raise UserError(_(
-                "You can not enter an attendance in a submitted timesheet. " +
-                "Ask your manager to reset it before adding attendance."
-            ))
+        if timesheet and timesheet.state != "draft":
+            raise UserError(
+                _(
+                    "You can not enter an attendance in a submitted timesheet. "
+                    + "Ask your manager to reset it before adding attendance."
+                )
+            )
         else:
-            checkin_tz_date = self._get_attendance_employee_tz(
-                date=self.check_in
-            )
-            checkout_tz_date = self._get_attendance_employee_tz(
-                date=self.check_out
-            )
-            if ((timesheet.date_start > checkin_tz_date or
-                 timesheet.date_end < checkin_tz_date) or
-                    checkout_tz_date and (timesheet.date_start >
-                                          checkout_tz_date or
-                                          timesheet.date_end <
-                                          checkout_tz_date)):
-                raise UserError(_(
-                    "You can not enter an attendance date " +
-                    "outside the current timesheet dates."))
+            checkin_tz_date = self._get_attendance_employee_tz(date=self.check_in)
+            checkout_tz_date = self._get_attendance_employee_tz(date=self.check_out)
+            if (
+                (
+                    timesheet.date_start > checkin_tz_date
+                    or timesheet.date_end < checkin_tz_date
+                )
+                or checkout_tz_date
+                and (
+                    timesheet.date_start > checkout_tz_date
+                    or timesheet.date_end < checkout_tz_date
+                )
+            ):
+                raise UserError(
+                    _(
+                        "You can not enter an attendance date "
+                        + "outside the current timesheet dates."
+                    )
+                )
