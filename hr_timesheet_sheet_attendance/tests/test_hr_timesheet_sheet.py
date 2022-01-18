01526e588660454b4c39439e783bc1f20fc4e513
Ernesto Tejeda <ernesto.tejeda@tecnativa.com>
2022-01-18 15:38:46 -0500

    [MIG] hr_timesheet_sheet_attendance: Migration to 14.0

--- hr_timesheet_sheet_attendance/tests/test_hr_timesheet_sheet.py
+++ hr_timesheet_sheet_attendance/tests/test_hr_timesheet_sheet.py
@@ -2,9 +2,7 @@ import datetime
 
 from odoo.exceptions import UserError
 
-from odoo.addons.hr_timesheet_sheet_attendance.tests.hr_timesheet_sheet_test_cases import (
-    HrTimesheetTestCases,
-)
+from .hr_timesheet_sheet_test_cases import HrTimesheetTestCases
 
 
 class TestHrTimesheetSheet(HrTimesheetTestCases):
