a71d5945858175acb59f9fa6e4896363b25f2dc3
Ernesto Tejeda <ernesto.tejeda@tecnativa.com>
2022-01-18 15:36:34 -0500

    [IMP] hr_timesheet_sheet_attendance: black, isort, prettier

--- hr_timesheet_sheet_attendance/tests/test_hr_attendance.py
+++ hr_timesheet_sheet_attendance/tests/test_hr_attendance.py
@@ -1,12 +1,13 @@
 import datetime
 
-from odoo.addons.hr_timesheet_sheet_attendance.tests.\
-    hr_timesheet_sheet_test_cases import HrTimesheetTestCases
-from odoo.exceptions import ValidationError, UserError
+from odoo.exceptions import UserError, ValidationError
+
+from odoo.addons.hr_timesheet_sheet_attendance.tests.hr_timesheet_sheet_test_cases import (
+    HrTimesheetTestCases,
+)
 
 
 class TestHrAttendance(HrTimesheetTestCases):
-
     def setUp(self):
         super(TestHrAttendance, self).setUp()
         checkInDate = datetime.datetime(2018, 12, 12, 10, 0, 0)
@@ -22,18 +23,20 @@ class TestHrAttendance(HrTimesheetTestCases):
             self.timesheet,
             self.attendance_1.sheet_id,
             "Error while computing sheet_id on attendance.\
-            \nMethod: _compute_sheet_id"
+            \nMethod: _compute_sheet_id",
         )
 
     def test_01_test_timezone_conversion(self):
         # check for _get_attendance_employee_tz
-        self.user_id.tz = 'Etc/GMT+12'
+        self.user_id.tz = "Etc/GMT+12"
         date = datetime.datetime(2018, 12, 12, 10, 0, 0)
         attDate = self.attendance_1._get_attendance_employee_tz(date=date)
         self.assertEqual(
-            attDate, datetime.date(2018, 12, 11),
+            attDate,
+            datetime.date(2018, 12, 11),
             "Error while converting date/datetime in user's timezone.\
-            \nMethod: _get_attendance_employee_tz")
+            \nMethod: _get_attendance_employee_tz",
+        )
 
     def test_02_check_timesheet_confirm(self):
         # check check_in & check_out equal or not
@@ -56,13 +59,17 @@ class TestHrAttendance(HrTimesheetTestCases):
 
         # modify attendance in confirmed timesheet
         with self.assertRaises(ValidationError):
-            self.attendance_1.write({
-                'check_in': datetime.datetime(2018, 12, 12, 14, 0, 0),
-            })
+            self.attendance_1.write(
+                {
+                    "check_in": datetime.datetime(2018, 12, 12, 14, 0, 0),
+                }
+            )
 
     def test_03_check_timesheet(self):
         # check when create attendance out_side the current timesheet date
         with self.assertRaises(ValidationError):
-            self.attendance_1.write({
-                'check_out': datetime.datetime(2018, 12, 16, 17, 0, 0),
-            })
+            self.attendance_1.write(
+                {
+                    "check_out": datetime.datetime(2018, 12, 16, 17, 0, 0),
+                }
+            )
