26bfcbd205213df796b9497918a34380bacae669
dsolanki <dhara.solanki@initos.com>
2022-01-21 17:57:34 +0530

    [IMP] hr_timesheet_report: black, isort, prettier

--- hr_timesheet_report/__manifest__.py
+++ hr_timesheet_report/__manifest__.py
@@ -3,25 +3,23 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Task Logs Timesheet Report',
-    'version': '12.0.1.0.0',
-    'category': 'Human Resources',
-    'maintainers': ['alexey-pelykh'],
-    'website': 'https://github.com/OCA/timesheet',
-    'author':
-        'CorporateHub, '
-        'Odoo Community Association (OCA)',
-    'license': 'AGPL-3',
-    'installable': True,
-    'application': False,
-    'summary': 'Generate Timesheet Report from Task Logs',
-    'depends': [
-        'hr_timesheet',
-        'report_xlsx',
+    "name": "Task Logs Timesheet Report",
+    "version": "12.0.1.0.0",
+    "category": "Human Resources",
+    "maintainers": ["alexey-pelykh"],
+    "website": "https://github.com/OCA/timesheet",
+    "author": "CorporateHub, " "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "installable": True,
+    "application": False,
+    "summary": "Generate Timesheet Report from Task Logs",
+    "depends": [
+        "hr_timesheet",
+        "report_xlsx",
     ],
-    'data': [
-        'views/account_analytic_line.xml',
-        'report/hr_timesheet_report.xml',
-        'wizards/hr_timesheet_report_wizard.xml',
+    "data": [
+        "views/account_analytic_line.xml",
+        "report/hr_timesheet_report.xml",
+        "wizards/hr_timesheet_report_wizard.xml",
     ],
 }
