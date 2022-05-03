5bab6804877ed4b86201931472d7c21708e37f0d
Juany Davila <juanyadavilap@gmail.com>
2022-05-03 09:03:11 -0500

    [MIG] hr_employee_product: Migration to 14.0

--- hr_employee_product/__manifest__.py
+++ hr_employee_product/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Manage products representing employees",
-    "version": "12.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Human Resources",
     "author": "Eficent, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/timesheet",
