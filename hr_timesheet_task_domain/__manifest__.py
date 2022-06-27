33e98967a7396ffecce234f525585624d762f949
Manuel Calero <manuelcalerosolis@gmail.com>
2022-06-27 18:38:05 +0200

    [MIG] hr_timesheet_task_domain: Migration to 15.0

--- hr_timesheet_task_domain/__manifest__.py
+++ hr_timesheet_task_domain/__manifest__.py
@@ -9,7 +9,7 @@
 
 {
     "name": "Task Log: limit Task by Project",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "category": "Human Resources",
     "website": "https://github.com/OCA/timesheet",
     "author": "Tecnativa, CorporateHub, Odoo Community Association (OCA)",
