2857c4e87e55092ecd6810b0d656600da22ca5e9
Manuel Calero <manuelcalerosolis@gmail.com>
2022-06-28 10:40:54 +0200

    [MIG] hr_timesheet_task_stage: Migration to 15.0

--- hr_timesheet_task_stage/__manifest__.py
+++ hr_timesheet_task_stage/__manifest__.py
@@ -8,7 +8,7 @@
 
 {
     "name": "Task Log: Open/Close Task",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "category": "Operations/Timesheets",
     "website": "https://github.com/OCA/timesheet",
     "author": "Tecnativa, CorporateHub, Odoo Community Association (OCA)",
