191118d0fc921da8d4dd875fe3a379225fd1296a
Hardik-OSI <hsuthar@opensourceintegrators.com>
2022-08-26 18:08:34 +0530

    [15.0][FIX] Pre-Commit

--- sale_timesheet_limit_date/models/account_move_line.py
+++ sale_timesheet_limit_date/models/account_move_line.py
@@ -5,26 +5,29 @@ from odoo import api, models
 
 
 class AccountMoveLine(models.Model):
-    _inherit = 'account.move.line'
+    _inherit = "account.move.line"
 
     @api.model_create_multi
     def create(self, values):
         # unlink lines from invoice if they under limit_date
         res = super().create(values)
-        to_clean = self.env['account.analytic.line']
+        to_clean = self.env["account.analytic.line"]
         for move_line in res:
-            if (move_line.move_id.move_type == 'out_invoice'
-                    and move_line.move_id.state == 'draft'
-                    and move_line.move_id.timesheet_limit_date):
+            if (
+                move_line.move_id.move_type == "out_invoice"
+                and move_line.move_id.state == "draft"
+                and move_line.move_id.timesheet_limit_date
+            ):
                 sale_line_delivery = move_line.sale_line_ids.filtered(
-                    lambda sol: sol.product_id.service_policy == 'delivered_timesheet'
-                    and sol.product_id.service_type == 'timesheet')
+                    lambda sol: sol.product_id.service_policy == "delivered_timesheet"
+                    and sol.product_id.service_type == "timesheet"
+                )
                 if sale_line_delivery:
-                    to_clean += self.env['account.analytic.line'].search([
-                        ('timesheet_invoice_id', '=',
-                            move_line.move_id.id),
-                        ('date', '>',
-                            move_line.move_id.timesheet_limit_date),
-                    ])
-        to_clean.write({'timesheet_invoice_id': False})
+                    to_clean += self.env["account.analytic.line"].search(
+                        [
+                            ("timesheet_invoice_id", "=", move_line.move_id.id),
+                            ("date", ">", move_line.move_id.timesheet_limit_date),
+                        ]
+                    )
+        to_clean.write({"timesheet_invoice_id": False})
         return res
