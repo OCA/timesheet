08cdebee2a56ca58d59ff5eac7e19519e88fb4b5
Juany Davila <juanyadavilap@gmail.com>
2022-10-13 13:28:29 -0500

    [MIG] hr_timesheet_sheet_period: Migration to 14.0

--- hr_timesheet_sheet_period/__manifest__.py
+++ hr_timesheet_sheet_period/__manifest__.py
@@ -1,13 +1,13 @@
-# Copyright 2016-17 Eficent Business and IT Consulting Services S.L.
+# Copyright 2016-17 ForgeFlow S.L.
 # Copyright 2016-17 Serpent Consulting Services Pvt. Ltd.
 # Copyright 2019 Brainbean Apps (https://brainbeanapps.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "HR Timesheet Sheet based on Payroll Period",
-    "version": "12.0.2.0.0",
+    "version": "14.0.1.0.0",
     "category": "Human Resources",
-    "author": "Eficent Business and IT Consulting Services S.L., "
+    "author": "ForgeFlow S.L., "
     "Serpent Consulting Services Pvt. Ltd., "
     "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/timesheet",
