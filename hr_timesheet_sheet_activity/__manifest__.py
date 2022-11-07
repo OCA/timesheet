dc4492eba13f344387dadeea9ed98b042558af53
Ilyas <irazor147@gmail.com>
2022-11-07 14:11:51 +0500

    [IMP] hr_timesheet_sheet_activity: black, isort, prettier

--- hr_timesheet_sheet_activity/__manifest__.py
+++ hr_timesheet_sheet_activity/__manifest__.py
@@ -3,25 +3,22 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'HR Timesheet Sheet Activities',
-    'version': '12.0.1.0.0',
-    'category': 'Human Resources',
-    'website': 'https://github.com/OCA/timesheet',
-    'author':
-        'CorporateHub, '
-        'Odoo Community Association (OCA)',
-    'license': 'AGPL-3',
-    'installable': True,
-    'application': False,
-    'summary': (
-        'Automatic activities related to submission and review of timesheet'
-        ' sheets'
+    "name": "HR Timesheet Sheet Activities",
+    "version": "12.0.1.0.0",
+    "category": "Human Resources",
+    "website": "https://github.com/OCA/timesheet",
+    "author": "CorporateHub, " "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "installable": True,
+    "application": False,
+    "summary": (
+        "Automatic activities related to submission and review of timesheet" " sheets"
     ),
-    'depends': [
-        'hr_timesheet_sheet',
+    "depends": [
+        "hr_timesheet_sheet",
     ],
-    'data': [
-        'data/hr_timesheet_sheet_activity_data.xml',
-        'views/mail_activity.xml',
+    "data": [
+        "data/hr_timesheet_sheet_activity_data.xml",
+        "views/mail_activity.xml",
     ],
 }
