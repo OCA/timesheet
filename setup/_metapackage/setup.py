f964da803c8b5f10198d68063e1c447179595bfa
CRogos <crogos@gmail.com>
2022-11-10 10:07:15 +0100

    [MIG] hr_timesheet_activity_begin_end: Update to 15.0

--- setup/_metapackage/setup.py
+++ setup/_metapackage/setup.py
@@ -9,13 +9,16 @@ setuptools.setup(
     version=version,
     install_requires=[
         'odoo-addon-hr_employee_product>=15.0dev,<15.1dev',
+        'odoo-addon-hr_timesheet_begin_end>=15.0dev,<15.1dev',
         'odoo-addon-hr_timesheet_sheet>=15.0dev,<15.1dev',
+        'odoo-addon-hr_timesheet_sheet_begin_end>=15.0dev,<15.1dev',
         'odoo-addon-hr_timesheet_task_domain>=15.0dev,<15.1dev',
         'odoo-addon-hr_timesheet_task_required>=15.0dev,<15.1dev',
         'odoo-addon-hr_timesheet_task_stage>=15.0dev,<15.1dev',
         'odoo-addon-hr_timesheet_time_type>=15.0dev,<15.1dev',
         'odoo-addon-sale_timesheet_line_exclude>=15.0dev,<15.1dev',
         'odoo-addon-sale_timesheet_task_exclude>=15.0dev,<15.1dev',
+
     ],
     classifiers=[
         'Programming Language :: Python',
