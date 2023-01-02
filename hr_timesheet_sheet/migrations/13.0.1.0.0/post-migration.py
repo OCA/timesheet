227cb732cdd962708c75b53bb689227831728b57
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2023-01-02 18:40:37 +0100

    [OU-FIX] hr_timesheet_sheet: Missing noupdate changes

--- hr_timesheet_sheet/migrations/13.0.1.0.0/post-migration.py
+++ hr_timesheet_sheet/migrations/13.0.1.0.0/post-migration.py
@@ -1,4 +1,5 @@
 # Copyright 2020 ForgeFlow
+# Copyright 2022 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from dateutil.rrule import DAILY, MONTHLY, WEEKLY
@@ -18,3 +19,6 @@ def res_company_sheet_range_map_values(env):
 @openupgrade.migrate()
 def migrate(env, version):
     res_company_sheet_range_map_values(env)
+    openupgrade.load_data(
+        env.cr, "hr_timesheet_sheet", "migrations/13.0.1.0.0/noupdate_changes.xml"
+    )
