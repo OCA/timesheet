0aa25072be8fdafb884edf71c397db175ca7f439
Ruchir Shukla <ruchir@bizzappdev.com>
2023-02-24 12:36:55 +0530

    [MIG][15.0]hr_timesheet_sheet_autodraft: Migration to 15.0

--- hr_timesheet_sheet_autodraft/__manifest__.py
+++ hr_timesheet_sheet_autodraft/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "HR Timesheet Sheet Auto-draft",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "category": "Human Resources",
     "website": "https://github.com/OCA/timesheet",
     "author": "CorporateHub, Odoo Community Association (OCA)",
