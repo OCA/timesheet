fb3e07589ef90cfebf74d5a1341a4f1be6c98d8f
Ilyas <irazor147@gmail.com>
2023-02-28 20:56:10 +0500

    [UPD] _compute_so_line aal

--- sale_timesheet_order_line_no_update/models/account_ananytic_line.py
+++ sale_timesheet_order_line_no_update/models/account_ananytic_line.py
@@ -4,6 +4,20 @@ from odoo import api, models
 class AccountAnalyticLine(models.Model):
     _inherit = "account.analytic.line"
 
+    @api.depends(
+        "task_id.sale_line_id",
+        "project_id.sale_line_id",
+        "project_id.allow_billable",
+        "employee_id",
+    )
+    def _compute_so_line(self):
+        skip_ids = self.filtered(lambda t: t.task_id and t.task_id.new_sale_line_id)
+        self -= skip_ids
+        if self:
+            super(
+                AccountAnalyticLine, self.filtered(lambda t: not t.is_so_line_edited)
+            )._compute_so_line()
+
     @api.model
     def _timesheet_determine_sale_line(self, task, employee, project):
         if task and task.allow_billable and task.sale_line_id:
@@ -25,3 +39,7 @@ class AccountAnalyticLine(models.Model):
                     self.so_line = self.task_id.new_sale_line_id
                 else:
                     super(AccountAnalyticLine, self)._onchange_task_id_employee_id()
+
+    def write(self, values):
+        res = super(AccountAnalyticLine, self).write(values)
+        return res
