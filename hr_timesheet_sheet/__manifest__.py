bb71bde0efa08fd31ffe4788b0ba80b1477cc20f
tarteo <d.sluijk@onestein.nl>
2023-03-21 16:11:30 +0100

    [MIG] hr_timesheet_sheet: Migration to 16.0
    [MIG] hr_timesheet_sheet: Migration to 16.0
    
    [MIG] hr_timesheet_sheet: Migration to 16.0
    
    [MIG] hr_timesheet_sheet: Migration to 15.0
    
    [MIG] hr_timesheet_sheet: Migration to 16.0
    
    [MIG] hr_timesheet_sheet: Migration to 16.0
    
    [MIG] hr_timesheet_sheet: Migration to 16.0
    
    [DON'T MERGE] test-requirements.txt
    
    fixup
    
    fixup
    
    fixup
    
    [FIX] 'Add new line' Button width

--- hr_timesheet_sheet/__manifest__.py
+++ hr_timesheet_sheet/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     "name": "HR Timesheet Sheet",
-    "version": "15.0.1.1.0",
+    "version": "16.0.1.0.0",
     "category": "Human Resources",
     "sequence": 80,
     "summary": "Timesheet Sheets, Activities",
@@ -26,10 +26,4 @@
         "views/account_analytic_line_views.xml",
         "views/res_config_settings_views.xml",
     ],
-    "assets": {
-        "web.assets_backend": [
-            "hr_timesheet_sheet/static/src/js/backend.js",
-            "hr_timesheet_sheet/static/src/scss/backend.scss",
-        ]
-    },
 }
