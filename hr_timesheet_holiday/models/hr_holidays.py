c7956047ebb626886b9de17d27ab79b2d3e253e5
Juany Davila <juanyadavilap@gmail.com>
2023-05-14 18:19:48 -0500

    [FIX] hr_timesheet_holiday: skip public holidays in between leave requests

--- hr_timesheet_holiday/models/hr_holidays.py
+++ hr_timesheet_holiday/models/hr_holidays.py
@@ -34,7 +34,9 @@ class HrHolidays(models.Model):
             raise UserError(_('No active projects for this Analytic Account'))
         # User exists because already checked during the action_approve
         user = self.employee_id.user_id
-        self.check_no_duplicate_leaves_hook(user, date, hours)
+        public = self.check_no_duplicate_leaves_hook(user, date, hours)
+        if public:
+            return
         self.sudo().with_context(force_write=True).write(
             {'analytic_line_ids': [(0, False, {
                 'name': description,
