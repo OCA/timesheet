f114d1e0e7abfb7c086d2f08208d806e97fb9df9
Maxime Franco <maxime.franco@acsone.eu>
2023-06-06 13:46:27 +0200

    [IMP] project_task_stage_allow_timesheet - pre-commit stuff

--- project_task_stage_allow_timesheet/tests/test_project_task_stage_allow_timesheet.py
+++ project_task_stage_allow_timesheet/tests/test_project_task_stage_allow_timesheet.py
@@ -2,29 +2,28 @@
 # Copyright 2019 Brainbean Apps (https://brainbeanapps.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo.tests.common import SavepointCase
 from odoo.exceptions import ValidationError
+from odoo.tests.common import SavepointCase
 
 
 class TestProjectTaskStageAllowTimesheet(SavepointCase):
-
     @classmethod
     def setUpClass(cls):
         super(TestProjectTaskStageAllowTimesheet, cls).setUpClass()
-        cls.AnalyticLine = cls.env['account.analytic.line']
+        cls.AnalyticLine = cls.env["account.analytic.line"]
 
-        cls.project_1 = cls.env.ref('project.project_project_1')
-        cls.task_1 = cls.env.ref('project.project_task_1')
+        cls.project_1 = cls.env.ref("project.project_project_1")
+        cls.task_1 = cls.env.ref("project.project_task_1")
 
-        cls.stage_new = cls.env.ref('project.project_stage_data_0')
+        cls.stage_new = cls.env.ref("project.project_stage_data_0")
 
     def test_01_stage_allow_timesheet(self):
         self.task_1.stage_id = self.stage_new
 
         values = {
-            'name': 'test',
-            'project_id': self.project_1.id,
-            'task_id': self.task_1.id,
+            "name": "test",
+            "project_id": self.project_1.id,
+            "task_id": self.task_1.id,
         }
         self.AnalyticLine.create(values)
 
