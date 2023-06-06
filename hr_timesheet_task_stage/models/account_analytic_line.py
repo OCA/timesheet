97a46a2bbeb7b0256358f5cebe6ea71e89d8fcc9
heliaktiv <heli.aktiv@gmail.com>
2023-06-06 17:46:00 +0530

    [MIG] hr_timesheet_task_stage: Migration to 16.0
    Co-Authored-By:
    Stefan Ungureanu
    heliaktiv

--- hr_timesheet_task_stage/models/account_analytic_line.py
+++ hr_timesheet_task_stage/models/account_analytic_line.py
@@ -12,16 +12,14 @@ from odoo.exceptions import UserError
 class AccountAnalyticLine(models.Model):
     _inherit = "account.analytic.line"
 
-    is_task_closed = fields.Boolean(related="task_id.stage_id.is_closed")
+    is_task_closed = fields.Boolean(related="task_id.stage_id.fold")
 
     def action_open_task(self):
-        ProjectTaskType = self.env["project.task.type"]
-
         for line in self.filtered("task_id.project_id"):
-            stage = ProjectTaskType.search(
+            stage = self.env["project.task.type"].search(
                 [
                     ("project_ids", "=", line.task_id.project_id.id),
-                    ("is_closed", "=", False),
+                    ("fold", "=", False),
                 ],
                 limit=1,
             )
@@ -35,14 +33,9 @@ class AccountAnalyticLine(models.Model):
             line.task_id.write({"stage_id": stage.id})
 
     def action_close_task(self):
-        ProjectTaskType = self.env["project.task.type"]
-
         for line in self.filtered("task_id.project_id"):
-            stage = ProjectTaskType.search(
-                [
-                    ("project_ids", "=", line.task_id.project_id.id),
-                    ("is_closed", "=", True),
-                ],
+            stage = self.env["project.task.type"].search(
+                [("project_ids", "=", line.task_id.project_id.id), ("fold", "=", True)],
                 limit=1,
             )
             if not stage:  # pragma: no cover
