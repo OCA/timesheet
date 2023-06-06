4a62bfc02b60fa9d87bd24fd0698212bc1e4d731
Maxime Franco <maxime.franco@acsone.eu>
2023-06-06 13:46:27 +0200

    [IMP] project_task_stage_allow_timesheet - pre-commit stuff

--- project_task_stage_allow_timesheet/__manifest__.py
+++ project_task_stage_allow_timesheet/__manifest__.py
@@ -3,19 +3,19 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Project Task Stage Allow Timesheet',
-    'summary': """
+    "name": "Project Task Stage Allow Timesheet",
+    "summary": """
         Allows to tell that a task stage is opened for timesheets.""",
-    'version': '12.0.1.0.1',
-    'license': 'AGPL-3',
-    'author': 'Odoo Community Association (OCA), ACSONE SA/NV',
-    'website': 'https://github.com/OCA/timesheet',
-    'depends': [
-        'hr_timesheet',
-        'project',
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Odoo Community Association (OCA), ACSONE SA/NV",
+    "website": "https://github.com/OCA/timesheet",
+    "depends": [
+        "hr_timesheet",
+        "project",
     ],
-    'data': [
-        'views/account_analytic_line.xml',
-        'views/project_task_type.xml',
+    "data": [
+        "views/account_analytic_line.xml",
+        "views/project_task_type.xml",
     ],
 }
