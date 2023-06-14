a0cf294842d174c49e23e17b45bc0f05b77fafaa
len <nans.lefebvre@gmail.com>
2023-06-14 09:51:42 +0200

    [FIX] hr_timesheet_sheet: adapt tests to 16.0

--- hr_timesheet_sheet/tests/test_hr_timesheet_sheet.py
+++ hr_timesheet_sheet/tests/test_hr_timesheet_sheet.py
@@ -8,7 +8,7 @@ from datetime import date
 from dateutil.relativedelta import relativedelta
 
 from odoo import fields
-from odoo.exceptions import AccessError, UserError, ValidationError
+from odoo.exceptions import UserError, ValidationError
 from odoo.tests.common import Form, TransactionCase
 
 from ..models.hr_timesheet_sheet import empty_name
@@ -952,7 +952,8 @@ class TestHrTimesheetSheet(TransactionCase):
         analytic_account = sheet.timesheet_ids.account_id
         self.assertEqual(analytic_account.company_id, self.company)
 
-        with self.assertRaises(AccessError):
+        # breaks consistency check on analytic line
+        with self.assertRaises(UserError):
             analytic_account.company_id = self.company_2
 
     def test_16(self):
@@ -981,10 +982,15 @@ class TestHrTimesheetSheet(TransactionCase):
                 "department_id": department.id,
             }
         )
-        sheet_form = Form(self.sheet_model.with_user(self.user))
-        sheet_form.employee_id = new_employee
-        sheet_form.department_id = self.department_model
-        sheet_no_department = sheet_form.save()
+        # the form forbids writing on an invisible field,
+        # so we create it manually instead to test the onchange
+        vals = {
+            "employee_id": new_employee.id,
+            "department_id": False,
+            "company_id": self.company.id,
+        }
+        model = self.sheet_model.with_user(self.user)
+        sheet_no_department = model.create(vals)
         self.assertFalse(sheet_no_department.department_id)
         sheet_no_department._onchange_employee_id()
         self.assertTrue(sheet_no_department.department_id)
@@ -1029,8 +1035,8 @@ class TestHrTimesheetSheet(TransactionCase):
     def test_workflow(self):
         sheet = Form(self.sheet_model.with_user(self.user)).save()
 
-        self.sheet_model.with_user(self.user).fields_view_get(view_type="form")
-        self.sheet_model.with_user(self.user).fields_view_get(view_type="tree")
+        self.sheet_model.with_user(self.user).get_view(view_type="form")
+        self.sheet_model.with_user(self.user).get_view(view_type="tree")
 
         with self.assertRaises(UserError):
             sheet.with_user(self.user_3).action_timesheet_refuse()
