fd66ea6b1458c86a17efc557d037aca962d72d1f
JasminSForgeFlow <jasmin.solanki@forgeflow.com>
2023-08-01 17:50:54 +0530

    [MIG] hr_timesheet_sheet_attendance: Migration to 15.0

--- hr_timesheet_sheet_attendance/__manifest__.py
+++ hr_timesheet_sheet_attendance/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "HR Timesheet Sheet Attendance",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "category": "Human Resources",
     "sequence": 80,
     "license": "AGPL-3",
