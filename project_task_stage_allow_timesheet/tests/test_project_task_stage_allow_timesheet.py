77182393c00b03314c60c08a74755b6298dcb20f
sergiocorato <sergiocorato@gmail.com>
2023-09-06 18:21:39 +0200

    [14.0] Migration of project_task_stage_allow_timesheet

--- project_task_stage_allow_timesheet/tests/test_project_task_stage_allow_timesheet.py
+++ project_task_stage_allow_timesheet/tests/test_project_task_stage_allow_timesheet.py
@@ -15,7 +15,7 @@ class TestProjectTaskStageAllowTimesheet(SavepointCase):
         cls.project_1 = cls.env.ref("project.project_project_1")
         cls.task_1 = cls.env.ref("project.project_task_1")
 
-        cls.stage_new = cls.env.ref("project.project_stage_data_0")
+        cls.stage_new = cls.env.ref("project.project_stage_0")
 
     def test_01_stage_allow_timesheet(self):
         self.task_1.stage_id = self.stage_new
@@ -28,5 +28,10 @@ class TestProjectTaskStageAllowTimesheet(SavepointCase):
         self.AnalyticLine.create(values)
 
         self.stage_new.allow_timesheet = False
-        with self.assertRaises(ValidationError), self.env.cr.savepoint():
+        with self.assertRaises(ValidationError) as e, self.env.cr.savepoint():
             self.AnalyticLine.create(values)
+
+        self.assertIn(
+            "You can't link a timesheet line to a task if its stage doesn't allow it.",
+            e.exception.name,
+        )
