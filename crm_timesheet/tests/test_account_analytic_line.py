5dedaf2984a90f125f3b2a27568324273817b815
Carolina Fernandez <carolina.fernandez@tecnativa.com>
2023-09-25 08:00:33 -0300

    [MIG] crm_timesheet: Migration to 16.0

--- crm_timesheet/tests/test_account_analytic_line.py
+++ crm_timesheet/tests/test_account_analytic_line.py
@@ -1,4 +1,5 @@
 # Copyright 2017 Jairo Llopis <jairo.llopis@tecnativa.com>
+# Copyright 2023 Tecnativa - Carolina Fernandez
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 
@@ -19,16 +20,17 @@ class AccountAnalyticLineCase(TransactionCase):
                 ("unit_amount", "=", 0),
             ]
         ).button_end_work()
-        admin.groups_id |= cls.env.ref(
-            "hr_timesheet.group_hr_timesheet_user"
-        ) | cls.env.ref("sales_team.group_sale_salesman")
+        admin.groups_id |= (
+            cls.env.ref("hr_timesheet.group_hr_timesheet_user")
+            | cls.env.ref("sales_team.group_sale_salesman")
+            | cls.env.ref("project.group_project_manager")
+        )
         env = cls.env(user=admin)
         Account = env["account.analytic.account"]
         Project = env["project.project"]
+        cls.analytic_plan = cls.env["account.analytic.plan"].create({"name": "Default"})
         cls.account1 = Account.create(
-            {
-                "name": "Test Account 1",
-            }
+            {"name": "Test Account 1", "plan_id": cls.analytic_plan.id}
         )
         cls.project1 = Project.create(
             {
