f74bc4398b7809ffd44fad007d95fcc599ee7bc8
LauraCForgeFlow <laura.cazorla@forgeflow.com>
2024-04-10 15:36:09 +0200

    [MIG] hr_timesheet_sheet_attendance: Migration to 16.0

--- hr_timesheet_sheet_attendance/tests/hr_timesheet_sheet_test_cases.py
+++ hr_timesheet_sheet_attendance/tests/hr_timesheet_sheet_test_cases.py
@@ -5,53 +5,57 @@ from odoo.tests.common import TransactionCase
 
 
 class HrTimesheetTestCases(TransactionCase):
-    def setUp(self):
-        super(HrTimesheetTestCases, self).setUp()
-        self.user_id = self._create_user()
-        self.employee = self._create_employee(self.user_id)
-        self.timesheet = self._create_timesheet_sheet(
-            self.employee, datetime.date(2018, 12, 12)
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.user_id = cls._create_user()
+        cls.employee = cls._create_employee(cls.user_id)
+        cls.timesheet = cls._create_timesheet_sheet(
+            cls.employee, datetime.date(2018, 12, 12)
         )
-        self.project_id = self.env.ref("project.project_project_1")
-        self.task_1 = self.env.ref("project.project_task_1")
+        cls.project_id = cls.env.ref("project.project_project_1")
+        cls.task_1 = cls.env.ref("project.project_1_task_9")
 
-    def _create_user(self):
+    @classmethod
+    def _create_user(cls):
         """Create and return user"""
         user_vals = {
             "name": "TestUser",
             "login": "test",
             "password": "test",
-            "company_id": self.env.ref("base.main_company").id,
+            "company_id": cls.env.ref("base.main_company").id,
             "groups_id": [
                 (
                     6,
                     0,
                     [
-                        self.env.ref("base.group_user").id,
-                        self.env.ref("base.group_partner_manager").id,
+                        cls.env.ref("base.group_user").id,
+                        cls.env.ref("base.group_partner_manager").id,
                     ],
                 )
             ],
         }
-        return self.env["res.users"].create(user_vals)
+        return cls.env["res.users"].create(user_vals)
 
-    def _create_employee(self, user):
+    @classmethod
+    def _create_employee(cls, user):
         """Create employee
         :param user: record set of res.user
         :param return: recordset of hr.employee"""
         employee_vals = {
             "name": "TestEmployee",
-            "user_id": self.user_id.id,
-            "department_id": self.env.ref("hr.dep_rd").id,
-            "job_id": self.env.ref("hr.job_developer").id,
-            "category_ids": [(6, 0, [self.env.ref("hr.employee_category_4").id])],
-            "work_location_id": self.env.ref("hr.work_location_1").id,
+            "user_id": cls.user_id.id,
+            "department_id": cls.env.ref("hr.dep_rd").id,
+            "job_id": cls.env.ref("hr.job_developer").id,
+            "category_ids": [(6, 0, [cls.env.ref("hr.employee_category_4").id])],
+            "work_location_id": cls.env.ref("hr.work_location_1").id,
             "work_email": "test@test.com",
             "work_phone": "+3281813700",
         }
-        return self.env["hr.employee"].create(employee_vals)
+        return cls.env["hr.employee"].create(employee_vals)
 
-    def _create_timesheet_sheet(self, employee, date=None):
+    @classmethod
+    def _create_timesheet_sheet(cls, employee, date=None):
         """Create employee
         :param employee: record set of hr.employee
         :param str date: date
@@ -63,9 +67,10 @@ class HrTimesheetTestCases(TransactionCase):
             "date_start": date,
             "date_end": date,
         }
-        return self.env["hr_timesheet.sheet"].create(sheet_vals)
+        return cls.env["hr_timesheet.sheet"].create(sheet_vals)
 
-    def _create_attendance(self, employee, checkIn=None, checkOut=None):
+    @classmethod
+    def _create_attendance(cls, employee, checkIn=None, checkOut=None):
         """Create employee
         :param employee: record set of hr.employee
         :param str checkIn: datetime
@@ -77,4 +82,4 @@ class HrTimesheetTestCases(TransactionCase):
         }
         if checkOut:
             attendance_vals.update({"check_out": checkOut})
-        return self.env["hr.attendance"].create(attendance_vals)
+        return cls.env["hr.attendance"].create(attendance_vals)
