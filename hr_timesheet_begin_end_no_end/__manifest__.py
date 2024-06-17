76488b173cec0c2226d1c621e0c4f9caaf9c8e83
Carmen Bianca BAKKER <carmen@coopiteasy.be>
2024-06-17 17:06:38 +0200

    [ADD] hr_timesheet_begin_end_no_end
    Signed-off-by: Carmen Bianca BAKKER <carmen@coopiteasy.be>

---
+++ hr_timesheet_begin_end_no_end/__manifest__.py
@@ -0,0 +1,18 @@
+# SPDX-FileCopyrightText: 2024 Coop IT Easy SC
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+{
+    "name": "Timesheet - Begin/End Hours with empty end",
+    "summary": (
+        "Allow to insert begin hour without end hour, which the user may fill"
+        " in later."
+    ),
+    "version": "12.0.1.0.0",
+    "category": "Human Resources",
+    "website": "https://github.com/OCA/timesheet",
+    "author": "Coop IT Easy SC, Odoo Community Association (OCA)",
+    "maintainers": ["carmenbianca"],
+    "license": "AGPL-3",
+    "depends": ["hr_timesheet_activity_begin_end"],
+}
