bd213816c3d02d2691fa2cfb2d0c1caf3d2bdfbd
Victor Champonnois <victor@coopiteasy.be>
2024-06-24 10:03:30 +0200

    [ADD]  hr_timesheet_editable_top

---
+++ hr_timesheet_editable_top/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2015 Camptocamp SA - Guewen Baconnier
+# Copyright 2017 Tecnativa, S.L. - Luis M. Ontalba
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "Timesheets - Edit on top",
+    "summary": "Add new timesheet entries to the top of the list",
+    "version": "16.0.1.0.0",
+    "author": "Coop IT Easy SC, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "category": "Human Resources",
+    "depends": ["hr_timesheet"],
+    "website": "https://github.com/OCA/timesheet",
+    "data": ["views/hr_analytic_timesheet.xml"],
+}
---
+++ hr_timesheet_editable_top/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+Add new timesheet entries on the top of the list instead of on the bottom. This is useful when quickly adding new timesheet entries.
