9e1f73a39c4afc0391714b18c65ade0cfbea2c54
hda <hughes.damry@acsone.eu>
2025-02-06 12:00:17 +0100

    [FIX] project_task_stage_allow_timesheet: invoice creation

--- project_task_stage_allow_timesheet/__manifest__.py
+++ project_task_stage_allow_timesheet/__manifest__.py
@@ -6,7 +6,7 @@
     "name": "Project Task Stage Allow Timesheet",
     "summary": """
         Allows to tell that a task stage is opened for timesheets.""",
-    "version": "16.0.1.0.0",
+    "version": "16.0.1.0.1",
     "license": "AGPL-3",
     "author": "Odoo Community Association (OCA), ACSONE SA/NV",
     "website": "https://github.com/OCA/timesheet",
