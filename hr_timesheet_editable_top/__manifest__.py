a2aef413298577e3f4d5f2584081becaf3a58cdd
Manuel Fombuena <fombuena@outlook.com>
2025-03-12 01:11:53 +0000

    [MIG] hr_timesheet_editable_top: Migration to 17.0

--- hr_timesheet_editable_top/__manifest__.py
+++ hr_timesheet_editable_top/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Timesheets - Edit on top",
     "summary": "Add new timesheet entries to the top of the list",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "author": "Coop IT Easy SC, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "category": "Human Resources",
--- hr_timesheet_editable_top/readme/CONTRIBUTORS.md
+++ hr_timesheet_editable_top/readme/CONTRIBUTORS.md
@@ -1,2 +1,4 @@
 - [Coop IT Easy SC](https://coopiteasy.be):
   - Victor Champonnois
+
+- Manuel Fombuena <mfombuena@innovara.tech>
