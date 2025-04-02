34dcc0eb1da13ad1c8f7d3b43707a09835708a67
Lansana Barry Sow <lbarry@apsl.net>
2025-04-02 08:26:50 +0200

    [16.0][ADD] hr_timesheet_date_order_desc: New module hr_timesheet_date_order_desc

---
+++ hr_timesheet_date_order_desc/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Timesheets Date Order Desc",
+    "summary": "Add new timesheet entries to the top of the list and order by date descending",
+    "version": "16.0.1.0.0",
+    "author": "Lansana Barry Sow, APSL-Nagarro, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "category": "Human Resources",
+    "depends": ["hr_timesheet"],
+    "website": "https://github.com/OCA/timesheet",
+    "data": ["views/hr_analytic_timesheet.xml"],
+}
---
+++ hr_timesheet_date_order_desc/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+Add new  to the top of the list, rather than the bottom, and sort by date in descending order. This is useful for quickly adding new timesheet entries.
