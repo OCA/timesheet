d4b4d1f5a6c9dd11d058b7a37bdaecd6e8d34bf7
Víctor Martínez <victor.martinez@tecnativa.com>
2025-04-14 08:58:19 +0200

    [FIX] crm_timesheet: Correctly hide lead_id column with column_invisible
    [BOT] post-merge updates

--- crm_timesheet/__manifest__.py
+++ crm_timesheet/__manifest__.py
@@ -9,7 +9,7 @@
 {
     "name": "CRM Timesheet",
     "category": "Customer Relationship Management",
-    "version": "17.0.1.0.0",
+    "version": "17.0.1.0.1",
     "depends": ["crm", "project_timesheet_time_control"],
     "data": [
         "security/ir.model.access.csv",
