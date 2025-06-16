17710cabe22f063173cfdf8a8e5c0faf1a61edbe
Lansana Barry Sow <lbarry@apsl.net>
2025-06-16 13:48:46 +0200

    [IMP] hr_timesheet_date_order_desc: pre-commit auto fixes

--- hr_timesheet_date_order_desc/__manifest__.py
+++ hr_timesheet_date_order_desc/__manifest__.py
@@ -1,6 +1,7 @@
 {
     "name": "Timesheets Date Order Desc",
-    "summary": "Add new timesheet entries to the top of the list and order by date descending",
+    "summary": "Add new timesheet entries to the top of the list and "
+    "order by date descending",
     "version": "16.0.1.0.0",
     "author": "Lansana Barry Sow, APSL-Nagarro, Odoo Community Association (OCA)",
     "license": "AGPL-3",
--- hr_timesheet_date_order_desc/readme/DESCRIPTION.rst
+++
@@ -1,1 +0,0 @@
-Add new  to the top of the list, rather than the bottom, and sort by date in descending order. This is useful for quickly adding new timesheet entries.
