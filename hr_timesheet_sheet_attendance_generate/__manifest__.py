1a4133a8040328dd8c11d71afa0ea9a0d0f66551
JasminSForgeFlow <jasmin.solanki@forgeflow.com>
2025-07-23 18:04:32 +0530

    [MIG] hr_timesheet_sheet_attendance_generate: Migration to 18.0

--- hr_timesheet_sheet_attendance_generate/__manifest__.py
+++ hr_timesheet_sheet_attendance_generate/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "HR Timesheet Sheet Attendance Generate",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Human Resources",
     "sequence": 80,
     "summary": "Timesheet Sheets, Activities",
