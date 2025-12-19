65539d029bddeb3b5a218d480aca7b54fb149a17
Slivi-sliv <silvano999@gmail.com>
2025-12-19 07:05:33 +0100

    Update test_timesheet_begin_end.py

--- hr_timesheet_begin_end/tests/test_timesheet_begin_end.py
+++ hr_timesheet_begin_end/tests/test_timesheet_begin_end.py
@@ -84,20 +84,20 @@ class TestBeginEnd(common.TransactionCase):
 
     def test_break_duration_calculation(self):
         vals = self.base_line.copy()
-        vals.update({
-            "time_start": 8.0,      # 08:00
-            "time_stop": 10.0,      # 10:00
-            "break_duration": 0.5,  # 30 min
-            "unit_amount": 1.5,     # (10-8-0.5)
-        })
+        vals.update(
+            {
+                "time_start": 8.0,  # 08:00
+                "time_stop": 10.0,  # 10:00
+                "break_duration": 0.5,  # 30 min
+                "unit_amount": 1.5,  # (10-8-0.5)
+            }
+        )
         line = self.timesheet_line_model.create(vals)
         self.assertEqual(line.unit_amount, 1.5)
 
     def test_onchange_with_break(self):
-        line = self.timesheet_line_model.new({
-            "time_start": 8.0,
-            "time_stop": 10.0,
-            "break_duration": 0.5
-        })
+        line = self.timesheet_line_model.new(
+            {"time_start": 8.0, "time_stop": 10.0, "break_duration": 0.5}
+        )
         line.onchange_hours_start_stop()
         self.assertEqual(line.unit_amount, 1.5)
