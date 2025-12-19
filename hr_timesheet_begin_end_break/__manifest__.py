9fc28e81963bd78a671d8cae930756e6dcd49fa0
Slivi-sliv <silvano999@gmail.com>
2025-12-19 14:32:15 +0100

    Add hr_timesheet_begin_end_break module

--- hr_timesheet_begin_end_break/__manifest__.py
+++ hr_timesheet_begin_end_break/__manifest__.py
@@ -1,12 +1,12 @@
 {
     "name": "Timesheet - Begin/End Hours + Break",
-    "version": "18.0.1.0.0",
+    "version": "18.0.1.0.9",
     "author": "S.S., Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "category": "Services/Timesheet",
-    "depends": ["hr_timesheet_begin_end"],
+    "depends": ["hr_timesheet","hr_timesheet_begin_end","project",],
     "website": "https://github.com/OCA/timesheet",
-    "data": ["views/hr_analytic_timesheet.xml",],
+    "data": ["views/hr_analytic_timesheet.xml","views/project_task_view.xml",],
     "installable": True,
     "summary": "Adds break duration to begin/end timesheets",
 }
