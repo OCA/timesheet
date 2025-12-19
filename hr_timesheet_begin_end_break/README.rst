ef10044c75296dbc3caa8a7e727b869dbddcb8eb
Slivi-sliv <silvano999@gmail.com>
2025-12-19 13:43:59 +0100

    Fix manifest, linting and add missing README
    Adjusted manifest authors, added website, fixed redundancy in models and added README.rst to comply with OCA standards.

---
+++ hr_timesheet_begin_end_break/README.rst
@@ -0,0 +1,5 @@
+==============================
+Timesheet - Begin/End Hours Break
+==============================
+
+This module adds a break duration field to timesheets when using begin/end hours.
--- hr_timesheet_begin_end_break/__manifest__.py
+++ hr_timesheet_begin_end_break/__manifest__.py
@@ -1,13 +1,12 @@
 {
-    "name": "Timesheet - Begin/End Hours Break",
-    "summary": "Adds break duration to begin/end timesheets",
+    "name": "Timesheet - Begin/End Hours + Break",
     "version": "18.0.1.0.0",
-    "category": "Services/Timesheet",
-    "author": "Slivi-sliv, OCA/Timesheet-publishers",
+    "author": "S.S., Odoo Community Association (OCA)",
     "license": "AGPL-3",
+    "category": "Services/Timesheet",
     "depends": ["hr_timesheet_begin_end"],
-    "data": [
-        "views/hr_analytic_timesheet.xml",
-    ],
+    "website": "https://github.com/OCA/timesheet",
+    "data": ["views/hr_analytic_timesheet.xml",],
     "installable": True,
+    "summary": "Adds break duration to begin/end timesheets",
 }
