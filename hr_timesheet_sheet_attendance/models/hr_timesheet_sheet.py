2d3fdf96c2afaedbf7441f2e9d435d95a2bbd1a9
Fanani M. Ihsan <fanani.mi@gmail.com>
2026-04-16 00:05:08 +0700

    [FIX] hr_timesheet_sheet_attendance: pre-commit check error

--- hr_timesheet_sheet_attendance/models/hr_timesheet_sheet.py
+++ hr_timesheet_sheet_attendance/models/hr_timesheet_sheet.py
@@ -1,5 +1,6 @@
-import pytz
 from datetime import datetime, time
+
+import pytz
 
 from odoo import _, api, fields, models
 from odoo.exceptions import UserError
@@ -95,12 +96,16 @@ class HrTimesheetSheet(models.Model):
         attendances = self.env["hr.attendance"]
         user_tz = pytz.timezone(self.env.user.tz or "UTC")
         for res in records:
-            date_start = user_tz.localize(
-                datetime.combine(res.date_start, time.min)
-            ).astimezone(pytz.UTC).replace(tzinfo=None)
-            date_end = user_tz.localize(
-                datetime.combine(res.date_end, time.max)
-            ).astimezone(pytz.UTC).replace(tzinfo=None)
+            date_start = (
+                user_tz.localize(datetime.combine(res.date_start, time.min))
+                .astimezone(pytz.UTC)
+                .replace(tzinfo=None)
+            )
+            date_end = (
+                user_tz.localize(datetime.combine(res.date_end, time.max))
+                .astimezone(pytz.UTC)
+                .replace(tzinfo=None)
+            )
             attendances |= self.env["hr.attendance"].search(
                 [
                     ("employee_id", "=", res.employee_id.id),
