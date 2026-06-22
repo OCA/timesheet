eb6930c9a9636641e12be66b20ab04e133125928
David Vidal <david@moduon.team>
2026-06-22 16:10:32 +0200

    [ADD] hr_timesheet_partner: new module
    MT-15111

---
+++ hr_timesheet_partner/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2026 Moduon Team S.L. <info@moduon.team>
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+{
+    "name": "HR Timesheet partner",
+    "summary": """Adds UX improvements to handle timesheet partner""",
+    "version": "18.0.1.0.0",
+    "development_status": "Beta",
+    "category": "Timesheet",
+    "website": "https://github.com/OCA/timesheet",
+    "author": "Moduon, Odoo Community Association (OCA)",
+    "maintainers": ["chienandalu", "rafaelbn"],
+    "license": "LGPL-3",
+    "depends": ["hr_timesheet"],
+    "data": [
+        "views/account_analytic_line_views.xml",
+    ],
+}
